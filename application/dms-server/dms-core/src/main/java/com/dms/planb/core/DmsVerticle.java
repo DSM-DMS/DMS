494966699515b30ec5f098c06786384959a21c30
JoMingyu <city7310@naver.com>
2017-01-26 21:12:41 +0900

    Modified verticle. Sorry Sorry Sorry..

--- application/dms-server/dms-core/src/main/java/com/dms/planb/core/DmsVerticle.java
+++ application/dms-server/dms-core/src/main/java/com/dms/planb/core/DmsVerticle.java
@@ -87,12 +87,18 @@ class DmsVerticle extends AbstractVerticle {
 					response = request.response();
 					response.putHeader("Content-type", "application/json; charset=utf-8");
 					
-					if(responseObject.getInt("status") == 1) {
-						response.setStatusCode(200);
-					} else {
-						response.setStatusCode(500);
-						// 500 : Internal Server Error
+					if(responseObject.containsKey("status")) {
+						if(responseObject.getInt("status") == 1) {
+							response.setStatusCode(200);
+						} else {
+							response.setStatusCode(500);
+							// 500 : Internal Server Error
+						}
 					}
+					else {
+						response.setStatusCode(200);
+					}
+
 					//	Log.l("Responsed status code : " + responseObject.getInt("status"));
 					responseObject.remove("status");
 					
