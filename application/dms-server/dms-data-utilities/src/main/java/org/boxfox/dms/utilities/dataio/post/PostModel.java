1e5437ecee466800d7f99befaa9d311f7c770c4d
boxfox <rlatjdfo112@naver.com>
2017-01-31 15:35:32 +0900

    add page post

--- application/dms-server/dms-data-utilities/src/main/java/org/boxfox/dms/utilities/dataio/post/PostModel.java
+++ application/dms-server/dms-data-utilities/src/main/java/org/boxfox/dms/utilities/dataio/post/PostModel.java
@@ -1,13 +1,35 @@
 package org.boxfox.dms.utilities.dataio.post;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.boxfox.dms.utilities.database.DataBase;
 import org.boxfox.dms.utilities.database.Query;
+import org.boxfox.dms.utilities.database.QueryUtils;
 import org.boxfox.dms.utilities.database.SafeResultSet;
 import org.boxfox.dms.utilities.datamodel.post.Post;
 
 public class PostModel {
+	private static String GET_POSTS_FROM_PAGES_QUERY = "select no, title, writer, date from app_content where category=? ORDER BY number asc limit ? 10";
+	
+	public static List<Post> getPostsAtPage(int category, int page){
+		ArrayList<Post> list = new ArrayList<Post>();
+		try {
+			SafeResultSet rs = DataBase.getInstance().executeQuery(QueryUtils.querySetter(GET_POSTS_FROM_PAGES_QUERY, category, page));
+			while(rs.next()){
+				Post post = new Post();
+				post.setNumber(rs.getInt("no"));
+				post.setTitle(rs.getString("title"));
+				post.setWriter(rs.getString("writer"));
+				post.setDateTime(rs.getString("date"));
+				list.add(post);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
 
 	public static Post getPost(int no) {
 		Post post = null;
