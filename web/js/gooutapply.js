33a673ee970ae166194a0d43035a3941b18c9259
DSMGun <geon0250@gmail.com>
2017-02-01 00:59:37 +0900

    changed go home apply input tag attribute and make go out apply

---
+++ web/js/gooutapply.js
@@ -0,0 +1,9 @@
+$('#date_picker').datepicker({
+  dateFormat: 'yy.MM.dd',
+  monthNames: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+  dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+  dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+  dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+  showMonthAfterYear: true,
+  yearSuffix: '년'
+});
