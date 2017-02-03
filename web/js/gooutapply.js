9284f944e8abd74d6610c19e5c395f73af6e8b78
DSMGun <geon0250@gmail.com>
2017-02-03 23:25:55 +0900

    change go out apply select method

--- web/js/gooutapply.js
+++ web/js/gooutapply.js
@@ -2,25 +2,25 @@ clickable = true;
 
 $('#selection').on("click", function () {
   if(clickable) {
-    $('#select_table').append('<tr><td id="sat" style="border-radius: 0 0 10% 10%">토요일</td></tr><tr><td id="sun" style="border-radius: 0 0 10% 10%">일요일</tr></td>');
+    $("#select_table td:eq(1), td:eq(2)").show();
     clickable = false;
 
     $("#sat").on("click", function () {
       $('#selection').text('토요일')
       $('#day').attr('value', '토요일');
-      $("#select_table tr:eq(1), tr:eq(2)").remove();
+      $("#select_table td:eq(1), td:eq(2)").hide();
       clickable = true;
     })
 
     $('#sun').on("click", function () {
       $('#selection').text('일요일');
       $('#day').attr('value', '일요일');
-      $("#select_table tr:eq(1), tr:eq(2)").remove();
+      $("#select_table td:eq(1), td:eq(2)").hide();
       clickable = true;
     })
 
   } else {
-    $("#select_table tr:eq(1), tr:eq(2)").remove();
+    $("#select_table td:eq(1), td:eq(2)").hide();
     clickable = true;
   }
 })
