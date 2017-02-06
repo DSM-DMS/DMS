fd9d35277038a88053f7d44898620dfb3a6f4523
graykick <graykick@naver.com>
2017-02-07 08:25:10 +0900

    implement extention apply
    implement extention apply

--- web/js/extentionapply.js
+++ web/js/extentionapply.js
@@ -1,88 +1,172 @@
-// ------------------------------- canvas
-
-var seatData = [
-    [null, null, "test", "조쯔위", "김사나"],
-    ["미사모", "손채영", "복숭아", null, "미나리"],
-    ["와이스", null, "급식단", null, "김다현"],
-    ["모모링", "박지효", "임나연", null, "룸메즈"]
-];
-
-
-function seat(owner, x, y) {
-    this.owner = owner;
-    this.x = x;
-    this.y = y;
-}
-
-//var objSeatArr = seatArrInit(seatData);
-
-function seatArrInit(seatArr) {
-    var objSeatArr = [];
-    for (var loop = 0; loop < seatArr.length; loop++) {
-        for (var innerLoop = 0; innerLoop < seatArr[0].length; innerLoop++) {
-            objSeatArr[loop][innerLoop] = new seat(seatArr[loop][innerLoop], innerLoop, loop);
-        }
-    }
-
-    return objSeatArr;
-}
-
-canvasInit();
-
-function canvasInit() {
-
-    //canvas의 사이즈를 컨테이너div에 맞게 조정함.
-    var canvasElement = $("#canvas");
-
-    //컨테이너div 사이즈의 픽셀값을 얻기위해 computedStyle객체 선언
-    var seat = document.querySelector(".seat");
-    var computedContainer = getComputedStyle(seat, null);
-
-    //컨테이너div의 computedContainer에 접근하여 얻은 픽셀 값을 이용하여 canvas 초기화
-    canvas.width = parseInt(computedContainer.getPropertyValue('width'), 10);
-    canvas.height = parseInt(computedContainer.getPropertyValue('height'), 10);
-}
-
-
-var canvasElement = document.getElementById("canvas");
-var ctx = canvasElement.getContext("2d");
-
-drawSeat(seatData);
+var selected;
+var selectableSeat = [];
 
 function drawSeat(seatArr) {
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvasElement.width, canvasElement.height);
+    for (var loop = 0; loop < seatArr.length; loop++) {
+        for (var innerLoop = 0; innerLoop < seatArr[0].length; innerLoop++) {
+            //draw circle
+            if (seatArr[loop][innerLoop] != 0) {
+                if (seatArr[loop][innerLoop] != 1) {
+                    $('<div/>', {
+                        "class": "seat",
+                        css: {
+                            // "border": "15px solid rgb(191,148,208)"
+                            "background": "rgb(134,193,233)"
+                        },
+                        text: seatArr[loop][innerLoop]
+                    }).appendTo(".seatcontainer");
+                } else {
+                    var newSeat = $('<div/>', {
+                        "class": "seat",
+                        css: {
+                            "border": "15px solid rgb(134,193,233)"
+                        },
+                    });
+                    newSeat.appendTo(".seatcontainer");
+                    newSeat.click(function() {
+                        if (selected !== undefined) {
+                            console.log(selected);
+                            selected.css({
+                                "background": "white",
+                                "border": "15px solid rgb(134,193,233)"
+                            });
+                            selected.text("");
+                        }
+                        console.log(selected);
+                        console.log($(this).get(0));
+                        if (selected !== undefined && selected.get(0) == this) {
+                            console.log("ASD");
+                            selected.css({
+                                "background": "rgb(231,160,153)",
+                                "border": "0 solid #FFD180"
+                            })
+                           selected.text("신청됨");
+                           selected = $(this);
+                        } else {
+                        console.log("wht");
+                        selected = $(this);
+                        $(this).css({
+                            "background": "#FFD180",
+                            "border": "0 solid #FFD180"
+                        });
+                        $(this).text("신청?");
+                         }
 
-    var interval = 10;
-    var seatWidth = ((canvasElement.width - interval) / 5);
-    var seatHeight = ((canvasElement.height - interval) / 4);
-    ctx.fillStyle = "white";
-    for (var loop = 0; loop < 4; loop++) {
-        for (var innerLoop = 0; innerLoop < 5; innerLoop++) {
-            if (seatArr[loop][innerLoop] === null) {
-                ctx.fillStyle = "gray";
-                drawRoundBorderRect(innerLoop * seatWidth + interval, loop * seatHeight + interval, seatWidth - interval, seatHeight - interval, 20, "gray");
+                    })
+                }
             } else {
-                ctx.font = "15px Arial";
-                ctx.textBaseline = "middle";
-                ctx.fillStyle = "red";
-                drawRoundBorderRect(innerLoop * seatWidth + interval, loop * seatHeight + interval, seatWidth - interval, seatHeight - interval, 20, "red");
-                ctx.fillStyle = "white";
-                console.log(ctx.measureText(seatArr[loop][innerLoop]).width);
-                console.log(seatWidth);
-                ctx.fillText(seatArr[loop][innerLoop], innerLoop * seatWidth + (seatWidth + interval - ctx.measureText(seatArr[loop][innerLoop]).width) / 2, loop * seatHeight + interval + (seatHeight - interval) / 2);
+                $('<div/>', {
+                    "class": "seat",
+                    css: {
+                        // "z-index": "100",
+                        // "background": "#f6f6f6",
+                        "border": "15px solid #757575"
+                    }
+                }).appendTo(".seatcontainer");
             }
         }
+        $('</br>').appendTo(".seatcontainer");
     }
 }
 
-function drawRoundBorderRect(rectX, rectY, rectWidth, rectHeight, cornerRadius, color) {
-    ctx.save();
-    ctx.lineJoin = "round";
-    ctx.lineWidth = cornerRadius;
-    ctx.fillStyle = color;
-    ctx.strokeStyle = color;
-    ctx.strokeRect(rectX + (cornerRadius / 2), rectY + (cornerRadius / 2), rectWidth - cornerRadius, rectHeight - cornerRadius);
-    ctx.fillRect(rectX + (cornerRadius / 2), rectY + (cornerRadius / 2), rectWidth - cornerRadius, rectHeight - cornerRadius);
-    ctx.restore();
-}
+var gaon = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 0],
+    [0, 1, 1, 0, 1, 1, 0],
+    [0, 1, 1, 0, 1, 1, 0],
+    [0, 1, 1, 0, 1, 1, 0],
+    [0, 1, 1, 0, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+];
+
+var gaon2 = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, "임나연", "조성빈", 0, "사나", "권장호", 0],
+    [0, "모모", "김지수", 0, "박지효", "박영준", 0],
+    [0, "미나", "구승완", 0, 1, 1, 0],
+    [0, 1, 1, 0, 1, 1, 0],
+    [0, 1, 1, 0, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+];
+
+var naon = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+];
+
+var naon2 = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, "루시우", "디바", "메르시", "트레이서", "정크렛", 0],
+    [0, "솔저", 0, "라인하르트", 0, "메이", 0],
+    [0, "맥크리", 0, "리퍼", 0, "겐지", 0],
+    [0, "아나", 0, "솜브라", 0, "젠야타", 0],
+    [0, "바스티온", 0, "한조", 0, "파라", 0],
+    [0, 0, 0, 0, 0, 0, 0]
+];
+
+var daon = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+];
+
+var daon2 = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, "호랑이", "사자", "사슴", "노루", "거북이", 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 1, "코끼리", "기린", 1, "코뿔소", 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 1, "하마", 1, "개미", 1, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, "펭귄", 1, "여우", 1, "늑대", 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, "수달", 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+];
+
+drawSeat(gaon2);
+
+
+// ====가온실====
+// □□□□□□□
+// □●●□●●□
+// □●●□●●□
+// □●●□●●□
+// □●●□●●□
+// □●●□●●□
+// □□□□□□□
+//
+// ====나온실====
+// □□□□□□□
+// □●●●●●□
+// □●□●□●□
+// □●□●□●□
+// □●□●□●□
+// □●□●□●□
+// □□□□□□□
+//
+// ====다온실====
+// □□□□□□□
+// □●●●●●□
+// □□□□□□□
+// □●●●●●□
+// □□□□□□□
+// □●●●●●□
+// □□□□□□□
+// □●●●●●□
+// □□□□□□□
+// □●●●●●□
+// □□□□□□□
