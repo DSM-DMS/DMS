a03fbde0d1ec9824c5a52174a356129bdcbb1b19
JoMingyu <city7310@naver.com>
2017-02-15 16:33:53 +0900

    Refreshed application version

--- application/dms-server/dms-core/src/main/java/com/dms/planb/core/ApplicationInfo.java
+++ application/dms-server/dms-core/src/main/java/com/dms/planb/core/ApplicationInfo.java
@@ -4,5 +4,5 @@ package com.dms.planb.core;
  * @author JoMingyu
  */
 public class ApplicationInfo {
-	public static final String VERSION = "1.0.7 TEST";
+	public static final String VERSION = "1.0.8 TEST";
 }
