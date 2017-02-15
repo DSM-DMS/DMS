6dcd0cf7b0dc892de85a083aef74534171320f6f
JoMingyu <city7310@naver.com>
2017-02-15 22:16:27 +0900

    Added SenderInfo class

---
+++ application/dms-server/dms-core/src/main/java/com/dms/planb/support/SenderInfo.java
@@ -0,0 +1,31 @@
+package com.dms.planb.support;
+
+/**
+ * @author KimSeongrae, JoMingyu
+ */
+public class SenderInfo {
+	private String userAgnet;
+	private String uuid;
+	private String version;
+	
+	public String getUserAgnet() {
+		return userAgnet;
+	}
+	public void setUserAgnet(String userAgnet) {
+		this.userAgnet = userAgnet;
+	}
+	
+	public String getUuid() {
+		return uuid;
+	}
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+	
+	public String getVersion() {
+		return version;
+	}
+	public void setVersion(String version) {
+		this.version = version;
+	}
+}
