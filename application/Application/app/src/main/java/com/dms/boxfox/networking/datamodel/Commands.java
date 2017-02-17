e7d2e86db5b46cdeed4320ad56e6e8f92e4a00c0
BeINone <sbeinone@gmail.com>
2017-02-17 13:08:01 +0900

    Fixed Commands

--- application/Application/app/src/main/java/com/dms/boxfox/networking/datamodel/Commands.java
+++ application/Application/app/src/main/java/com/dms/boxfox/networking/datamodel/Commands.java
@@ -15,76 +15,56 @@ public class Commands {
     /*
      * Notice, Newsletter, Competition, Plan, Meal will parsed in dms-data-utilities
      */
-    public static final int INSERT											= 1; // prefix
-
-    public static final int REGISTER_STUDENT_ACC			= 100;
-    public static final int REGISTER_TEACHER_ACC			= 101;
+    public static final int REGISTER_STUDENT_ACC				= 100;
+    public static final int REGISTER_TEACHER_ACC				= 101;
 
     // notice, newsletter, competition : parsing
-    public static final int UPLOAD_RULE								= 111;
-    public static final int UPLOAD_QNA_QUESTION					= 112;
-    public static final int UPLOAD_QNA_ANSWER						= 113;
-    public static final int UPLOAD_QNA_COMMENT			= 114;
-    public static final int UPLOAD_FAQ								= 115;
-    public static final int UPLOAD_AFTERSCHOOL_ITEM	= 116;
-    public static final int UPLOAD_REPORT_FACILITY		= 117;
-    public static final int UPLOAD_REPORT_RESULT 		= 118;
+    public static final int UPLOAD_RULE									= 111;
+    public static final int UPLOAD_QNA_QUESTION				= 112;
+    public static final int UPLOAD_QNA_ANSWER				= 113;
+    public static final int UPLOAD_QNA_COMMENT				= 114;
+    public static final int UPLOAD_FAQ									= 115;
+    public static final int UPLOAD_AFTERSCHOOL_ITEM		= 116;
+    public static final int UPLOAD_REPORT_FACILITY			= 117;
+    public static final int UPLOAD_REPORT_RESULT 			= 118;
 
-    public static final int APPLY_EXTENSION						= 131;
-    public static final int APPLY_STAY									= 132;
-    public static final int APPLY_GOINGOUT						= 133;
-    public static final int APPLY_MERIT								= 134;
-    public static final int APPLY_AFTERSCHOOL				= 135;
+    public static final int APPLY_AFTERSCHOOL					= 131;
 
 /* --------------------------------------------------------------------- */
 
-    public static final int UPDATE											= 2; // prefix
+    public static final int MODIFY_PASSWORD						= 200;
+    public static final int MODIFY_STUDENT_DATA				= 201;
 
-    public static final int MODIFY_PASSWORD					= 200;
-    public static final int MODIFY_STUDENT_DATA			= 201;
+    public static final int MODIFY_RULE									= 211;
+    public static final int MODIFY_QNA_QUESTION				= 212;
+    public static final int MODIFY_QNA_ANSWER					= 213;
+    public static final int MODIFY_QNA_COMMENT				= 214;
+    public static final int MODIFY_FAQ									= 215;
+    public static final int MODIFY_REPORT_FACILITY			= 216;
 
-    public static final int MODIFY_RULE								= 211;
-    public static final int MODIFY_QNA_QUESTION					= 212;
-    public static final int MODIFY_QNA_ANSWER						= 213;
-    public static final int MODIFY_QNA_COMMENT			= 214;
-    public static final int MODIFY_FAQ								= 215;
-    public static final int MODIFY_REPORT_FACILITY		= 216;
-
-    public static final int MODIFY_EXTENSION_APPLY					= 231;
-    public static final int MODIFY_STAY_APPLY								= 232;
-    public static final int MODIFY_STAY_DEFAULT			= 233;
-    public static final int MODIFY_GOINGOUT_APPLY					= 234;
-    public static final int MODIFY_MERIT_APPLY							= 235;
-    public static final int MODIFY_AFTERSCHOOL_APPLY				= 236;
+    public static final int MODIFY_AFTERSCHOOL_APPLY			= 231;
+    public static final int MODIFY_STAY_DEFAULT				= 232;
 
 /* --------------------------------------------------------------------- */
 
-    public static final int DELETE											= 3; // prefix
+    public static final int DELETE_ACCOUNT							= 300;
 
-    public static final int DELETE_ACCOUNT						= 300;
+    public static final int DELETE_RULE									= 311;
+    public static final int DELETE_QNA_QUESTION				= 312;
+    public static final int DELETE_QNA_ANSWER					= 313;
+    public static final int DELETE_QNA_COMMENT				= 314;
+    public static final int DELETE_FAQ										= 315;
+    public static final int DELETE_REPORT_FACILITY			= 316;
 
-    public static final int DELETE_RULE								= 311;
-    public static final int DELETE_QNA_QUESTION						= 312;
-    public static final int DELETE_QNA_ANSWER							= 313;
-    public static final int DELETE_QNA_COMMENT			= 314;
-    public static final int DELETE_FAQ									= 315;
-    public static final int DELETE_REPORT_FACILITY		= 316;
-
-    public static final int WITHDRAW_EXTENSION_APPLY					= 331;
-    public static final int WITHDRAW_GOINGOUT_APPLY					= 332;
-    public static final int WITHDRAW_MERIT_APPLY							= 333;
-	/*
-	 * Deapply : 기피하다
-	 * 철회하다 : Withdraw, Waive
-	 */
+    public static final int WITHDRAW_EXTENSION_APPLY	= 331;
+    public static final int WITHDRAW_GOINGOUT_APPLY	= 332;
+    public static final int WITHDRAW_MERIT_APPLY			= 333;
 
 /* --------------------------------------------------------------------- */
-
-    public static final int SELECT											= 4; // prefix
 
     public static final int LOAD_MYPAGE								= 401;
     //	public static final int LOAD_TEACHER_MYPAGE				= 402;
-    public static final int LOGIN_STUDENT_REQUEST							= 403;
+    public static final int LOGIN_STUDENT_REQUEST		= 403;
     public static final int LOGIN_TEACHER_REQUEST		= 404;
 
     public static final int LOAD_NOTICE_LIST						= 411;
@@ -114,4 +94,11 @@ public class Commands {
     public static final int LOAD_MEAL									= 437;
     public static final int LOAD_PLAN									= 438;
     public static final int LOAD_SCORE								= 439;
+
+/* --------------------------------------------------------------------- */
+
+    public static final int APPLY_EXTENSION						= 501;
+    public static final int APPLY_STAY									= 502;
+    public static final int APPLY_GOINGOUT						= 503;
+    public static final int APPLY_MERIT								= 504;
 }
