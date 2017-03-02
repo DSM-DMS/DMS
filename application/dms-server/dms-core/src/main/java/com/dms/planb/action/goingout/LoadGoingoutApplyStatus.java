23d2c235c3fbcf524f2f241a59fd7be461393b8f
JoMingyu <city7310@naver.com>
2017-03-02 17:29:47 +0900

    Fixed load goingout apply status' error

--- application/dms-server/dms-core/src/main/java/com/dms/planb/action/goingout/LoadGoingoutApplyStatus.java
+++ application/dms-server/dms-core/src/main/java/com/dms/planb/action/goingout/LoadGoingoutApplyStatus.java
@@ -12,15 +12,32 @@ import com.dms.planb.support.Commands;
 
 @ActionRegistration(command = Commands.LOAD_GOINGOUT_APPLY_STATUS)
 public class LoadGoingoutApplyStatus implements Actionable {
+	SafeResultSet resultSet;
+	EasyJsonObject tempObject;
+	
 	@Override
 	public EasyJsonObject action(Sender sender, int command, EasyJsonObject requestObject) throws SQLException {
 		String id = requestObject.getString("id");
 		
-		SafeResultSet resultSet = database.executeQuery("SELECT * FROM goingout_apply WHERE id='", id, "'");
+		try {
+			resultSet = database.executeQuery("SELECT * FROM goingout_apply WHERE id='", id, "'");
+		} catch(NullPointerException e) {
+			responseObject.put("status", 404);
+			
+			return responseObject;
+		}
 		
 		if(resultSet.next()) {
-			responseObject.put("date", resultSet.getBoolean("date"));
-			responseObject.put("reason", resultSet.getString("reason"));
+			do {
+				tempObject = new EasyJsonObject();
+				
+				tempObject.put("date", resultSet.getBoolean("date"));
+				tempObject.put("reason", resultSet.getString("reason"));
+				
+				array.add(tempObject);
+			} while(resultSet.next());
+			
+			responseObject.put("result", array);
 		} else {
 			responseObject.put("status", 404);
 		}
