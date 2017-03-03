74bc5a3f6691d9b61ac3b63ebab33e0c9118ed64
JoMingyu <city7310@naver.com>
2017-03-03 23:29:00 +0900

    Started changing REST

--- application/dms-server/dms-core/src/main/java/com/dms/planb/action/stay/LoadStayApplyStatus.java
+++ application/dms-server/dms-core/src/main/java/com/dms/planb/action/stay/LoadStayApplyStatus.java
@@ -2,15 +2,17 @@ package com.dms.planb.action.stay;
 
 import java.sql.SQLException;
 
-import org.boxfox.dms.utilities.actions.ActionRegistration;
-import org.boxfox.dms.utilities.actions.Actionable;
+import org.boxfox.dms.utilities.actions.RouteRegistration;
 import org.boxfox.dms.utilities.actions.support.Sender;
+import org.boxfox.dms.utilities.database.DataBase;
 import org.boxfox.dms.utilities.database.SafeResultSet;
 import org.boxfox.dms.utilities.json.EasyJsonObject;
 
-import com.dms.planb.support.Commands;
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.ext.web.RoutingContext;
 
-@ActionRegistration(command = Commands.LOAD_STAY_APPLY_STATUS)
+@RouteRegistration(path="apply/stay", method={HttpMethod.GET})
 public class LoadStayApplyStatus implements Handler<RoutingContext> {
 	EasyJsonObject tempObject;
 	
@@ -31,5 +33,29 @@ public class LoadStayApplyStatus implements Handler<RoutingContext> {
 		}
 		
 		return responseObject;
+	}
+
+	@Override
+	public void handle(RoutingContext context) {
+		SafeResultSet resultSet = null;
+		EasyJsonObject responseObject = new EasyJsonObject();
+		DataBase database = DataBase.getInstance();
+		
+		String id = context.request().getParam("id");
+		String week = context.request().getParam("week");
+		
+		try {
+			resultSet = database.executeQuery("SELECT * FROM stay_apply WHERE id='", id, "' AND week='", week, "'");
+			
+			if(resultSet.next()) {
+				responseObject.put("value", resultSet.getInt("value"));
+				
+				context.response().setStatusCode(200).end();
+			} else {
+				responseObject.put("status", 404);
+			}
+		} catch(SQLException e) {
+			
+		}
 	}
 }
--- application/dms-server/dms-core/src/main/java/com/dms/planb/action/stay/LoadStayDefault.java
+++ application/dms-server/dms-core/src/main/java/com/dms/planb/action/stay/LoadStayDefault.java
@@ -2,34 +2,41 @@ package com.dms.planb.action.stay;
 
 import java.sql.SQLException;
 
+import org.boxfox.dms.utilities.actions.RouteRegistration;
 import org.boxfox.dms.utilities.actions.support.Sender;
+import org.boxfox.dms.utilities.database.DataBase;
 import org.boxfox.dms.utilities.database.SafeResultSet;
 import org.boxfox.dms.utilities.json.EasyJsonObject;
 
-import com.dms.planb.support.Commands;
-
 import io.vertx.core.Handler;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.RoutingContext;
 
-@RouteRegist
+@RouteRegistration(path="apply/stay/default", method={HttpMethod.GET})
 public class LoadStayDefault implements Handler<RoutingContext> {
 	@Override
-	public EasyJsonObject action(Sender sender, int command, EasyJsonObject requestObject) throws SQLException {
-		String id = requestObject.getString("id");
+	public void handle(RoutingContext context) {
+		SafeResultSet resultSet = null;
+		EasyJsonObject responseObject = new EasyJsonObject();
+		DataBase database = DataBase.getInstance();
 		
-		SafeResultSet resultSet = database.executeQuery("SELECT * FROM stay_apply_default WHERE id='", id, "'");
+		String id = context.request().getParam("id");
 		
-		if(resultSet.next()) {
-			responseObject.put("value", resultSet.getString("value"));
-		} else {
-			responseObject.put("status", 404);
+		try {
+			resultSet = database.executeQuery("SELECT * FROM stay_apply_default WHERE id='", id, "'");
+			
+			if(resultSet.next()) {
+				responseObject.put("value", resultSet.getString("value"));
+				
+				context.response().setStatusCode(200).end();
+				context.response().end(responseObject.toString());
+				context.response().close();
+			} else {
+				context.response().setStatusCode(404).end();
+				context.response().close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-		
-		return responseObject;
-	}
-
-	@Override
-	public void handle(RoutingContext event) {
-		
 	}
 }
