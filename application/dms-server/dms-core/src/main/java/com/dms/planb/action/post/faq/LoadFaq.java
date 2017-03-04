c4c152c571548697e0933117041e66adfc1ddd7b
JoMingyu <city7310@naver.com>
2017-03-04 20:35:48 +0900

    Created LoadFaq class

---
+++ application/dms-server/dms-core/src/main/java/com/dms/planb/action/post/faq/LoadFaq.java
@@ -0,0 +1,46 @@
+package com.dms.planb.action.post.faq;
+
+import java.sql.SQLException;
+
+import org.boxfox.dms.utilities.actions.RouteRegistration;
+import org.boxfox.dms.utilities.database.DataBase;
+import org.boxfox.dms.utilities.database.SafeResultSet;
+import org.boxfox.dms.utilities.json.EasyJsonObject;
+import org.boxfox.dms.utilities.log.Log;
+
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.ext.web.RoutingContext;
+
+@RouteRegistration(path="post/faq", method={HttpMethod.GET})
+public class LoadFaq implements Handler<RoutingContext> {
+	@Override
+	public void handle(RoutingContext context) {
+		DataBase database = DataBase.getInstance();
+		SafeResultSet resultSet;
+		EasyJsonObject responseObject = new EasyJsonObject();
+		
+		int no = Integer.parseInt(context.request().getParam("no"));
+		
+		try {
+			resultSet = database.executeQuery("SELECT * FROM faq WHERE no=", no);
+			
+			if(resultSet.next()) {
+				responseObject.put("title", resultSet.getString("title"));
+				responseObject.put("content", resultSet.getString("content"));
+				
+				context.response().setStatusCode(200).end();
+				context.response().end(responseObject.toString());
+				context.response().close();
+			} else {
+				context.response().setStatusCode(404).end();
+				context.response().close();
+			}
+		} catch(SQLException e) {
+			context.response().setStatusCode(500).end();
+			context.response().close();
+			
+			Log.l("SQLException");
+		}
+	}
+}
