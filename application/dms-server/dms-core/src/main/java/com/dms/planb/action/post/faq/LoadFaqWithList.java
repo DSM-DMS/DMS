2316d7fcb57e15ec49fed9cfdd8146e69d8b0e92
JoMingyu <city7310@naver.com>
2017-03-04 15:04:28 +0900

    Changed all of faq classes

--- application/dms-server/dms-core/src/main/java/com/dms/planb/action/post/faq/LoadFaqWithList.java
+++ application/dms-server/dms-core/src/main/java/com/dms/planb/action/post/faq/LoadFaqWithList.java
@@ -2,54 +2,66 @@ package com.dms.planb.action.post.faq;
 
 import java.sql.SQLException;
 
-import org.boxfox.dms.utilities.actions.ActionRegistration;
-import org.boxfox.dms.utilities.actions.Actionable;
-import org.boxfox.dms.utilities.actions.support.Sender;
+import org.boxfox.dms.utilities.actions.RouteRegistration;
+import org.boxfox.dms.utilities.database.DataBase;
 import org.boxfox.dms.utilities.database.SafeResultSet;
+import org.boxfox.dms.utilities.json.EasyJsonArray;
 import org.boxfox.dms.utilities.json.EasyJsonObject;
+import org.boxfox.dms.utilities.log.Log;
 
-import com.dms.planb.support.Commands;
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.ext.web.RoutingContext;
 
-@ActionRegistration(command = Commands.LOAD_FAQ)
+@RouteRegistration(path="post/faq/list", method={HttpMethod.GET})
 public class LoadFaqWithList implements Handler<RoutingContext> {
-	EasyJsonObject tempObject;
-	SafeResultSet resultSet;
-	
 	@Override
-	public EasyJsonObject action(Sender sender, int command, EasyJsonObject requestObject) throws SQLException {
-		// Both list and content
-		if(requestObject.isEmpty()) {
-			resultSet = database.executeQuery("SELECT * FROM faq");
-			/*
-			 * Responses all of posts
-			 */
-		} else {
-			int page = requestObject.getInt("page");
-			int limit = requestObject.getInt("limit");
-			
-			resultSet = database.executeQuery("SELECT * FROM faq limit ", ((page - 1) * limit), ", ", limit);
-		}
+	public void handle(RoutingContext context) {
+		DataBase database = DataBase.getInstance();
+		SafeResultSet resultSet;
+		EasyJsonObject responseObject = new EasyJsonObject();
+		EasyJsonObject tempObject = new EasyJsonObject();
+		EasyJsonArray tempArray = new EasyJsonArray();
 		
-		int postCount = 0;
-		if(resultSet.next()) {
-			do {
-				tempObject = new EasyJsonObject();
+		try {
+			if(!context.request().params().contains("page") && !context.request().params().contains("limit")) {
+				resultSet = database.executeQuery("SELECT * FROM faq");
+			} else {
+				int page = Integer.parseInt(context.request().getParam("page"));
+				int limit = Integer.parseInt(context.request().getParam("limit"));
 				
-				tempObject.put("no", resultSet.getInt("no"));
-				tempObject.put("title", resultSet.getString("title"));
-				tempObject.put("content", resultSet.getString("content"));
-				
-				array.add(tempObject);
-				
-				postCount++;
-			} while(resultSet.next());
+				resultSet = database.executeQuery("SELECT * FROM faq limit ", ((page - 1) * limit), ", ", limit);
+			}
 			
-			responseObject.put("num_of_post", postCount);
-			responseObject.put("result", array);
-		} else {
-			responseObject.put("status", 404);
+			int postCount = 0;
+			if(resultSet.next()) {
+				do {
+					tempObject = new EasyJsonObject();
+					
+					tempObject.put("no", resultSet.getInt("no"));
+					tempObject.put("title", resultSet.getString("title"));
+					tempObject.put("content", resultSet.getString("content"));
+					
+					tempArray.add(tempObject);
+					
+					postCount++;
+				} while(resultSet.next());
+				
+				responseObject.put("num_of_post", postCount);
+				responseObject.put("result", tempArray);
+				
+				context.response().setStatusCode(200).end();
+				context.response().end(responseObject.toString());
+				context.response().close();
+			} else {
+				context.response().setStatusCode(404).end();
+				context.response().close();
+			}
+		} catch(SQLException e) {
+			context.response().setStatusCode(500).end();
+			context.response().close();
+			
+			Log.l("SQLException");
 		}
-		
-		return responseObject;
 	}
 }
