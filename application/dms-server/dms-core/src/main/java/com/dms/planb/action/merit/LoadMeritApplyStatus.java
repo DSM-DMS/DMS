acbcf9c4015d69b429ffee64dff661a6ca95f895
JoMingyu <city7310@naver.com>
2017-03-04 15:42:17 +0900

    Changed all of merit classes

--- application/dms-server/dms-core/src/main/java/com/dms/planb/action/merit/LoadMeritApplyStatus.java
+++ application/dms-server/dms-core/src/main/java/com/dms/planb/action/merit/LoadMeritApplyStatus.java
@@ -2,34 +2,50 @@ package com.dms.planb.action.merit;
 
 import java.sql.SQLException;
 
-import org.boxfox.dms.utilities.actions.ActionRegistration;
-import org.boxfox.dms.utilities.actions.Actionable;
-import org.boxfox.dms.utilities.actions.support.Sender;
+import org.boxfox.dms.utilities.actions.RouteRegistration;
+import org.boxfox.dms.utilities.database.DataBase;
 import org.boxfox.dms.utilities.database.SafeResultSet;
 import org.boxfox.dms.utilities.json.EasyJsonObject;
+import org.boxfox.dms.utilities.log.Log;
 
-import com.dms.planb.support.Commands;
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.ext.web.RoutingContext;
 
-@ActionRegistration(command = Commands.LOAD_MERIT_APPLY_STATUS)
+@RouteRegistration(path="apply/merit", method={HttpMethod.GET})
 public class LoadMeritApplyStatus implements Handler<RoutingContext> {
 	@Override
-	public EasyJsonObject action(Sender sender, int command, EasyJsonObject requestObject) throws SQLException {
-		String id = requestObject.getString("id");
+	public void handle(RoutingContext context) {
+		DataBase database = DataBase.getInstance();
+		SafeResultSet resultSet;
+		EasyJsonObject responseObject = new EasyJsonObject();
 		
-		SafeResultSet resultSet = database.executeQuery("SELECT * FROM merit_apply WHERE id='", id, "'");
+		String id = context.request().getParam("id");
 		
-		if(resultSet.next()) {
-			responseObject.put("content", resultSet.getString("content"));
-			if(!resultSet.getString("target").isEmpty()) {
-				responseObject.put("has_target", true);
-				responseObject.put("target", resultSet.getString("target"));
+		try {
+			resultSet = database.executeQuery("SELECT * FROM merit_apply WHERE id='", id, "'");
+			
+			if(resultSet.next()) {
+				responseObject.put("content", resultSet.getString("content"));
+				if(!resultSet.getString("target").isEmpty()) {
+					responseObject.put("has_target", true);
+					responseObject.put("target", resultSet.getString("target"));
+				} else {
+					responseObject.put("has_target", false);
+				}
+				
+				context.response().setStatusCode(200).end();
+				context.response().end(responseObject.toString());
+				context.response().close();
 			} else {
-				responseObject.put("has_target", false);
+				context.response().setStatusCode(404).end();
+				context.response().close();
 			}
-		} else {
-			responseObject.put("status", 404);
+		} catch(SQLException e) {
+			context.response().setStatusCode(500).end();
+			context.response().close();
+			
+			Log.l("SQLException");
 		}
-		
-		return responseObject;
 	}
 }
--- application/dms-server/dms-core/src/main/java/com/dms/planb/action/merit/WithdrawMeritApply.java
+++ application/dms-server/dms-core/src/main/java/com/dms/planb/action/merit/WithdrawMeritApply.java
@@ -2,23 +2,32 @@ package com.dms.planb.action.merit;
 
 import java.sql.SQLException;
 
-import org.boxfox.dms.utilities.actions.ActionRegistration;
-import org.boxfox.dms.utilities.actions.Actionable;
-import org.boxfox.dms.utilities.actions.support.Sender;
-import org.boxfox.dms.utilities.json.EasyJsonObject;
+import org.boxfox.dms.utilities.actions.RouteRegistration;
+import org.boxfox.dms.utilities.database.DataBase;
+import org.boxfox.dms.utilities.log.Log;
 
-import com.dms.planb.support.Commands;
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.ext.web.RoutingContext;
 
-@ActionRegistration(command = Commands.WITHDRAW_MERIT_APPLY)
+@RouteRegistration(path="apply/merit", method={HttpMethod.DELETE})
 public class WithdrawMeritApply implements Handler<RoutingContext> {
 	@Override
-	public EasyJsonObject action(Sender sender, int command, EasyJsonObject requestObject) throws SQLException {
-		String id = requestObject.getString("id");
+	public void handle(RoutingContext context) {
+		DataBase database = DataBase.getInstance();
 		
-		int status = database.executeUpdate("DELETE FROM merit_apply WHERE id='", id, "'");
+		String id = context.request().getParam("id");
 		
-		responseObject.put("status", status);
-		
-		return responseObject;
+		try {
+			database.executeUpdate("DELETE FROM merit_apply WHERE id='", id, "'");
+			
+			context.response().setStatusCode(200).end();
+			context.response().close();
+		} catch(SQLException e) {
+			context.response().setStatusCode(500).end();
+			context.response().close();
+			
+			Log.l("SQLException");
+		}
 	}
 }
