805eb268e5d926d29704aa13ad21aaf9f01e1a22
graykick <graykick@naver.com>
2017-03-11 21:43:06 +0900

    add qnacomment.js at WEB-INF
    add qnacomment.js at WEB-INF

--- application/dms-server/WEB-INF/js/editor.js
+++ application/dms-server/WEB-INF/js/editor.js
@@ -1,3 +1,9 @@
 // Replace the <textarea id="editor1"> with a CKEditor
 // instance, using default configuration.
 CKEDITOR.replace('editor1');
+
+function setWriteBtnEvent() {
+    $(".editor-button").click(function () {
+        // 수정요청코드
+    })
+}
