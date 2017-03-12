72666c6ffb2aceafae972854cea8a8d4f764839d
JoMingyu <city7310@naver.com>
2017-03-12 09:11:56 +0900

    CORS 헤더 수정

--- application/dms-server/dms-core/src/main/java/com/dms/planb/support/PrecedingWork.java
+++ application/dms-server/dms-core/src/main/java/com/dms/planb/support/PrecedingWork.java
@@ -7,7 +7,7 @@ import io.vertx.ext.web.RoutingContext;
 public class PrecedingWork {
 	public static RoutingContext putHeaders(RoutingContext context) {
 		context.response().putHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type");
-		context.response().putHeader("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE, OPTIONS, CONNECT");
+		context.response().putHeader("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE, OPTIONS, HEAD, CONNECT");
 		context.response().putHeader("Access-Control-Allow-Origin", "http://dsm2015.cafe24.com/*");
 		context.response().putHeader("Access-Control-Allow-Origin", "http://dsm2015.cafe24.com/");
 		context.response().putHeader("Access-Control-Allow-Origin", "http://dsm2015.cafe24.com");
