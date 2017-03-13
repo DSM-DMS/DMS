2dc8a44d067db92224940ebe230e101e373881de
JoMingyu <city7310@naver.com>
2017-03-13 21:31:09 +0900

    연장신청 쿼리문 수정

--- application/dms-server/dms-core/src/main/java/com/dms/planb/action/extension/ApplyExtension.java
+++ application/dms-server/dms-core/src/main/java/com/dms/planb/action/extension/ApplyExtension.java
@@ -36,7 +36,12 @@ public class ApplyExtension implements Handler<RoutingContext> {
         String classId = context.request().getParam("class");
         String seatId = context.request().getParam("seat");
         String id = userManager.getIdFromSession(context);
-        String key = userManager.getUid(userManager.getIdFromSession(context));
+        String key = null;
+		try {
+			key = userManager.getUid(userManager.getIdFromSession(context));
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
         if (Guardian.checkParameters(classId, id, seatId)) {
             try {
                 String name = ((Map<String, Object>) userManager.getUserInfo(id).getArgs()[0]).get("name").toString();
@@ -49,8 +54,8 @@ public class ApplyExtension implements Handler<RoutingContext> {
                     context.response().setStatusCode(404).end();
                     context.response().close();
                 } else {
-                    database.executeUpdate("DELETE FROM extension_apply WHERE uid='", uid, "'");
-                    database.executeUpdate("INSERT INTO extension_apply(class, seat, name, uid) VALUES(", classId, ", ", seatId, ", '", name, "', '", uid, "')");
+                    database.executeUpdate("DELETE FROM extension_apply WHERE uid='", key, "'");
+                    database.executeUpdate("INSERT INTO extension_apply(class, seat, name, uid) VALUES(", classId, ", ", seatId, ", '", name, "', '", key, "')");
 
                     context.response().setStatusCode(200).end();
                     context.response().close();
