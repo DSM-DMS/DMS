94f3a0e4453b69aef0536fbd0fd6e373d8c05533
boxfox <rlatjdfo112@naver.com>
2017-03-15 19:22:21 +0900

    외출신청

--- application/dms-server/WEB-INF/js/go_out_apply.js
+++ application/dms-server/WEB-INF/js/go_out_apply.js
@@ -1,55 +1,29 @@
-$("#sat_go").flip();
-$("#sun_go").flip();
+$('#sat_go').flip();
+$('#sun_go').flip();
 
-var checkSat = function () {
-  var val = $("#sat_go").data("flip-model");
-  if(val.isFlipped) {
+var fillSat = function () {
+  var val = ${status_1?c};
+  if(val) {
     $("#sat_val").text('외출 X');
   } else {
     $("#sat_val").text('외출 O');
   }
 }
 
-var checkSun = function () {
-  var val = $("#sun_go").data("flip-model");
-  if(val.isFlipped) {
+var fillSun = function () {
+  var val = ${status_2?c};
+  if(val) {
     $("#sun_val").text('외출 X');
   } else {
     $("#sun_val").text('외출 O');
   }
 }
 
-checkSat();
-checkSun();
+$('#sat_go').flip(${status_1?c});
+$('#sun_go').flip(${status_2?c});
 
+fillSat();
+fillSun();
 
-$("#sat_go").on('flip:done',function(){
-  checkSat();
-});
-
-$("#sun_go").on('flip:done',function(){
-  checkSun();
-});
-
-
-$("#go_submit").on('click', function () {
-  var satVal = false;
-  var sunVal = false;
-  if($("#sat_val").text() == '외출 O') {
-    satVal = true;
-  }
-  if($("#sun_val").text() == '외출 O') {
-    sunVal = true;
-  }
-  $.ajax({
-    url: "/apply/goingout",
-    type: "PUT",
-    data: {
-      "sat": satVal,
-      "sun": sunVal
-    },
-    success: function() {
-      alert('신청되었습니다.');
-    }
-  });
-});
+$('#sat_go .back').removeAttr('hidden');
+$('#sun_go .back').removeAttr('hidden');
