c2246a0074bce45ed4c1ee44de3e88190ed60b11
DSMGun <geon0250@gmail.com>
2017-03-15 16:39:44 +0900

    Modify go out status change method

--- application/dms-server/WEB-INF/js/go_out_apply.js
+++ application/dms-server/WEB-INF/js/go_out_apply.js
@@ -1,41 +1,44 @@
 $("#sat_go").flip();
 $("#sun_go").flip();
 
-$.ajax({
-  url: "http://dsm2015.cafe24.com:8089/apply/goingout",
-  type: "get",
-  async: false,
-  success: function(data) {
-    if(data.sat) {
-      $("#sat_val").text('외출 O');
-    } else {
-      $("#sat_val").text('외출 X');
-      $("#sat_go").flip(true);
-    }
-    if(data.sun) {
-      $("#sun_val").text('외출 O');
-    } else {
-      $("#sun_val").text('외출 X');
-      $("#sun_go").flip(true);
-    }
+// $.ajax({
+//   url: "http://dsm2015.cafe24.com:8089/apply/goingout",
+//   type: "get",
+//   async: false,
+//   success: function(data) {
+//     if(data.sat) {
+//       $("#sat_val").text('외출 O');
+//     } else {
+//       $("#sat_val").text('외출 X');
+//       $("#sat_go").flip(true);
+//     }
+//     if(data.sun) {
+//       $("#sun_val").text('외출 O');
+//     } else {
+//       $("#sun_val").text('외출 X');
+//       $("#sun_go").flip(true);
+//     }
+//   }
+// });
+
+$("#sat_go").on('flip:done',function(){
+  var val = $("#sat_go").data("flip-model")
+  if(val.isFlipped) {
+    $("#sat_val").text('외출 X');
+  } else {
+    $("#sat_val").text('외출 O');
   }
 });
 
-$("#sat_go .front").on('click', function () {
-  $("#sat_val").text('외출 X');
+$("#sun_go").on('flip:done',function(){
+  var val = $("#sun_go").data("flip-model")
+  if(val.isFlipped) {
+    $("#sun_val").text('외출 X');
+  } else {
+    $("#sun_val").text('외출 O');
+  }
 });
 
-$("#sat_go .back").on('click', function () {
-  $("#sat_val").text('외출 O');
-});
-
-$("#sun_go .front").on('click', function () {
-  $("#sun_val").text('외출 X');
-});
-
-$("#sun_go .back").on('click', function () {
-  $("#sun_val").text('외출 O');
-});
 
 $("#go_submit").on('click', function () {
   var satVal = false;
