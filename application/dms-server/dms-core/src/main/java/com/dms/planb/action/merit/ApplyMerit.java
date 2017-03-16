4502c5c71b82c33810d25b2a60e6fe2f7bdda205
JoMingyu <city7310@naver.com>
2017-03-16 22:50:10 +0900

    faq checkParameters 생성

--- application/dms-server/dms-core/src/main/java/com/dms/planb/action/merit/ApplyMerit.java
+++ application/dms-server/dms-core/src/main/java/com/dms/planb/action/merit/ApplyMerit.java
@@ -2,6 +2,7 @@ package com.dms.planb.action.merit;
 
 import java.sql.SQLException;
 
+import org.boxfox.dms.util.Guardian;
 import org.boxfox.dms.util.UserManager;
 import org.boxfox.dms.utilities.actions.RouteRegistration;
 import org.boxfox.dms.utilities.database.DataBase;
@@ -30,12 +31,19 @@ public class ApplyMerit implements Handler<RoutingContext> {
 		String id = userManager.getIdFromSession(context);
         String uid = null;
         try {
-            if (id != null)
+            if (id != null) {
                 uid = userManager.getUid(id);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
 		String content = context.request().getParam("content");
+		
+		if(!Guardian.checkParameters(id, uid, content)) {
+            context.response().setStatusCode(400).end();
+            context.response().close();
+        	return;
+        }
 		
 		try {
 			if(context.request().params().contains("target")) {
