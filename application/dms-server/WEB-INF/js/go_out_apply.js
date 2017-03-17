87172042ebfe058f6d1b9b56366bfef2817b9c29
DSMGun <geon0250@gmail.com>
2017-03-17 13:52:50 +0900

    Fixed go_out_apply.js

--- application/dms-server/WEB-INF/js/go_out_apply.js
+++ application/dms-server/WEB-INF/js/go_out_apply.js
@@ -1,43 +1,61 @@
-<<<<<<< HEAD
-$('#sat_go').flip();
-$('#sun_go').flip();
-=======
 $("#sat_go").flip();
 $("#sun_go").flip();
 $("#sat_go").flip(satBool);
 $("#sun_go").flip(sunBool);
 $("#sat_go .back").removeAttr('hidden');
 $("#sun_go .back").removeAttr('hidden');
-<<<<<<< HEAD
->>>>>>> 46c7e1bcbd1fe3465ec3964c0d5760d37544783c
-=======
 $("#sat_go .front").removeAttr('hidden');
 $("#sun_go .front").removeAttr('hidden');
->>>>>>> d1e3ffb27823fd2a9475559a393febbd8682bc49
 
-var fillSat = function () {
-  var val = ${status_1?c};
-  if(val) {
+var checkSat = function () {
+  var val = $("#sat_go").data("flip-model");
+  if(val.isFlipped) {
     $("#sat_val").text('외출 X');
   } else {
     $("#sat_val").text('외출 O');
   }
 };
 
-var fillSun = function () {
-  var val = ${status_2?c};
-  if(val) {
+var checkSun = function () {
+  var val = $("#sun_go").data("flip-model");
+  if(val.isFlipped) {
     $("#sun_val").text('외출 X');
   } else {
     $("#sun_val").text('외출 O');
   }
 };
 
-$('#sat_go').flip(${status_1?c});
-$('#sun_go').flip(${status_2?c});
+checkSat();
+checkSun();
 
-fillSat();
-fillSun();
 
-$('#sat_go .back').removeAttr('hidden');
-$('#sun_go .back').removeAttr('hidden');
+$("#sat_go").on('flip:done',function(){
+  checkSat();
+});
+
+$("#sun_go").on('flip:done',function(){
+  checkSun();
+});
+
+
+$("#go_submit").on('click', function () {
+  var satVal = false;
+  var sunVal = false;
+  if($("#sat_val").text() == '외출 O') {
+    satVal = true;
+  }
+  if($("#sun_val").text() == '외출 O') {
+    sunVal = true;
+  }
+  $.ajax({
+    url: "/apply/goingout",
+    type: "PUT",
+    data: {
+      "sat": satVal,
+      "sun": sunVal
+    },
+    success: function() {
+      alert('신청되었습니다.');
+    }
+  });
+});
