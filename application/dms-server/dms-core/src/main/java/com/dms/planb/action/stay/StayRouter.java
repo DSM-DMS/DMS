48d7bf76a7fa76b1a1dc55a57c43d992cd45ecb5
JoMingyu <city7310@naver.com>
2017-03-18 00:46:45 +0900

    연장신청 페이지 route 생성

---
+++ application/dms-server/dms-core/src/main/java/com/dms/planb/action/stay/StayRouter.java
@@ -0,0 +1,45 @@
+package com.dms.planb.action.stay;
+
+import java.io.IOException;
+
+import org.boxfox.dms.util.UserManager;
+import org.boxfox.dms.utilities.actions.RouteRegistration;
+import org.boxfox.dms.utilities.log.Log;
+
+import com.dms.boxfox.templates.DmsTemplate;
+
+import freemarker.template.TemplateException;
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.ext.web.RoutingContext;
+
+@RouteRegistration(path="/stay", method={HttpMethod.GET})
+public class StayRouter implements Handler<RoutingContext> {
+private UserManager userManager;
+	
+	public StayRouter() {
+		userManager = new UserManager();
+	}
+	
+	public void handle(RoutingContext context) {
+		boolean isLogin = userManager.isLogined(context);
+		if(isLogin) {
+			DmsTemplate templates = new DmsTemplate("stay_apply");
+			try {
+				context.response().setStatusCode(200);
+				context.response().end(templates.process());
+				context.response().close();
+			} catch(IOException e) {
+				e.printStackTrace();
+				Log.l("IOException");
+			} catch(TemplateException e) {
+				Log.l("TemplateException");
+			}
+		} else {
+			context.response().setStatusCode(200);
+            context.response().putHeader("Content-type", "text/html; utf-8");
+            context.response().end("<script>window.location.href='/'</script>");
+            context.response().close();
+		}
+	}
+}
