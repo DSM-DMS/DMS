ac7b32bc3fbaa2463e597c836a560913efe6b327
JoMingyu <city7310@naver.com>
2017-03-18 19:15:52 +0900

    잔류신청 엑셀파일 다운로드 기능 추가, 테스트 후 재커밋

--- application/dms-server/dms-core/src/main/java/com/dms/planb/action/extension/ExtensionDownloadRouter.java
+++ application/dms-server/dms-core/src/main/java/com/dms/planb/action/extension/ExtensionDownloadRouter.java
@@ -4,15 +4,15 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.sql.SQLException;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.boxfox.dms.util.UserManager;
 import org.boxfox.dms.utilities.actions.RouteRegistration;
+import org.boxfox.dms.utilities.database.DataBase;
 
 import com.dms.planb.support.PrecedingWork;
 
@@ -22,55 +22,92 @@ import io.vertx.ext.web.RoutingContext;
 
 @RouteRegistration(path = "/extension/download", method={HttpMethod.POST})
 public class ExtensionDownloadRouter implements Handler<RoutingContext> {
-	UserManager userManager;
-	private static final String FORMAT_XLSX_FILE = "연장신청.xlsx";
-    private static final String FILE = "files/";
+	private static final String FORMAT_XLSX_FILE = "잔류조사포맷.xlsx";
+    private static final String FILE_DIR = "files/";
     
-    public ExtensionDownloadRouter() {
-    	userManager = new UserManager();
-    }
-	
 	@Override
 	public void handle(RoutingContext context) {
+		DataBase database = DataBase.getInstance();
+		
 		context = PrecedingWork.putHeaders(context);
 		
 		File file = getFile();
-		
+
 		try {
+			System.out.println(file.getPath());
 			XSSFWorkbook wb = new XSSFWorkbook(new FileInputStream(file));
-			
+
 			XSSFSheet sheet = wb.getSheetAt(0);
 			
-			for(Row row : sheet) {
-				for(Cell cell : row) {
-					switch(cell.getCellType()) {
+			int cnt = 0;
+			
+			for (Row row : sheet) {
+				for (Cell cell : row) {
+					switch (cell.getCellType()) {
 					case Cell.CELL_TYPE_NUMERIC:
-						String number = cell.getStringCellValue();
+						StringBuilder sb = new StringBuilder(Double.toString(cell.getNumericCellValue()));
+						sb.insert(1, "0");
+						
+						int number = Integer.valueOf(sb.toString());
+						String uid = database.executeQuery("SELECT * FROM student_data WHERE number=", number).getString("uid");
+						int classId = database.executeQuery("SELECT * FROM extension_apply WHERE uid='", uid, "'").getInt("class");
+						
+						// 연장신청 안된 경우 수정
+						
+						String className = null;
+						
+						switch(classId) {
+						case 1:
+							className = "가온실";
+							break;
+						case 2:
+							className = "나온실";
+							break;
+						case 3:
+							className = "다온실";
+							break;
+						case 4:
+							className = "라온실";
+							break;
+						case 5:
+							className = "3층 독서실";
+							break;
+						case 6:
+							className = "4층 독서실";
+							break;
+						case 7:
+							className = "5층 열린교실";
+							break;
+						default:
+							className = "미신청";
+							break;
+						}
+						
+						XSSFRow extensionStateRow = sheet.getRow(cell.getRowIndex());
+						Cell extensionStateCell = extensionStateRow.getCell(cell.getColumnIndex() + 2);
+						extensionStateCell.setCellValue(className);
+						
+						break;
 					}
 				}
 			}
-		} catch (IOException e) {
+			
+			FileOutputStream xlsToSave = new FileOutputStream(FILE_DIR + "잔류신청.xlsx");
+			wb.write(xlsToSave);
+			xlsToSave.close();
+			
+			context.response().setStatusCode(201);
+			context.response().sendFile(FILE_DIR + "잔류신청.xlsx");
+			context.response().close();
+		} catch (IOException | SQLException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	private File getFile() {
-        File file = new File(FILE + FORMAT_XLSX_FILE);
-        try {
-            if (!file.exists()) {
-                file.mkdir();
-                InputStream inputStream = getClass().getResourceAsStream(FILE + FORMAT_XLSX_FILE);
-                OutputStream outStream = new FileOutputStream(file);
-                int len = 0;
-                byte[] buf = new byte[1024];
-                while ((len = inputStream.read(buf)) > 0) {
-                    outStream.write(buf, 0, len);
-                }
-                outStream.close();
-                inputStream.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+		File file = new File(FILE_DIR + FORMAT_XLSX_FILE);
+        if (!file.exists()) {
+            file.mkdir();
         }
         return file;
     }
