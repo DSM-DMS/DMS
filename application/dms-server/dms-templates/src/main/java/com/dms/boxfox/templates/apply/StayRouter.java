feb94fb34183d1822f874606f4dd9fc6a1ae1e26
JoMingyu <city7310@naver.com>
2017-03-20 08:56:00 +0900

    stayapply router 수정

--- application/dms-server/dms-templates/src/main/java/com/dms/boxfox/templates/apply/StayRouter.java
+++ application/dms-server/dms-templates/src/main/java/com/dms/boxfox/templates/apply/StayRouter.java
@@ -14,7 +14,7 @@ import io.vertx.core.Handler;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.RoutingContext;
 
-@RouteRegistration(path="/stay", method={HttpMethod.GET})
+@RouteRegistration(path="/stayapply", method={HttpMethod.GET})
 public class StayRouter implements Handler<RoutingContext> {
 private UserManager userManager;
 	
