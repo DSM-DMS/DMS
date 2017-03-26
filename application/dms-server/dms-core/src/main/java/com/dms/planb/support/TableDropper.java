828dd6a36f5152f065355910d6a2329b04495d1b
PlanB <city7310@naver.com>
2017-03-26 16:10:40 +0900

    TableDropper 내부 구문이 1번만 실행되는 오류 수정

--- application/dms-server/dms-core/src/main/java/com/dms/planb/support/TableDropper.java
+++ application/dms-server/dms-core/src/main/java/com/dms/planb/support/TableDropper.java
@@ -7,28 +7,31 @@ import org.boxfox.dms.utilities.database.DataBase;
 
 public class TableDropper extends Thread {
 	public void run() {
-		Calendar currentTime = Calendar.getInstance();
-		int dayOfWeek = currentTime.get(Calendar.DAY_OF_WEEK);
-		int hour = currentTime.get(Calendar.HOUR_OF_DAY);
-		if (dayOfWeek == Calendar.MONDAY) {
-			try {
-				DataBase.getInstance().executeUpdate("delete from goingout_apply");
-				/*
-				 * Every Monday, refresh goingout_apply table
-				 */
-			} catch (SQLException e) {
-				e.printStackTrace();
+		while(true) {
+			Calendar currentTime = Calendar.getInstance();
+			int dayOfWeek = currentTime.get(Calendar.DAY_OF_WEEK);
+			int hour = currentTime.get(Calendar.HOUR_OF_DAY);
+			if (dayOfWeek == Calendar.MONDAY) {
+				try {
+					DataBase.getInstance().executeUpdate("delete from goingout_apply");
+					/*
+					 * Every Monday, refresh goingout_apply table
+				 	*/
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 			}
-		}
-		if (hour >= 0 && hour <= 8) {
-			try {
-				DataBase.getInstance().executeUpdate("delete from extension_apply");
-				/*
-				 * Every day, refresh extension_apply table
-				 */
-			} catch (SQLException e) {
-				e.printStackTrace();
+			if (hour >= 0 && hour <= 8) {
+				try {
+					DataBase.getInstance().executeUpdate("delete from extension_apply");
+					/*
+					 * Every day, refresh extension_apply table
+				 	*/
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 			}
+			Thread.sleep(3600000)
 		}
 	}
 }
