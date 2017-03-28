b663f8e2cf73ba9a19f736b426d08a741a42db60
JoMingyu <city7310@naver.com>
2017-03-28 16:17:49 +0900

    디버깅용 print문 삭제

--- application/dms-server/dms-secure/src/main/java/org/boxfox/dms/util/UserManager.java
+++ application/dms-server/dms-secure/src/main/java/org/boxfox/dms/util/UserManager.java
@@ -244,7 +244,6 @@ public class UserManager {
     }
 
     public static boolean isAdmin(RoutingContext ctx) {
-    	System.out.println("Branched");
         boolean check = false;
         String sessionKey = getRegistredSessionKey(ctx);
         try {
