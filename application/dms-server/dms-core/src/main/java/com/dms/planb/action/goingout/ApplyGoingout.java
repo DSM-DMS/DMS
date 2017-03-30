899a5e5f68fba2a9778a50c5448ffa09b8d6cd24
JoMingyu <city7310@naver.com>
2017-03-30 15:15:25 +0900

    외출신청, 엑셀파일 고쳤다 만세

--- application/dms-server/dms-core/src/main/java/com/dms/planb/action/goingout/ApplyGoingout.java
+++ application/dms-server/dms-core/src/main/java/com/dms/planb/action/goingout/ApplyGoingout.java
@@ -34,14 +34,14 @@ public class ApplyGoingout implements Handler<RoutingContext> {
         if (Guardian.checkParameters(sat, sun) && userManager.isLogined(context)) {
             try {
                 String uid = userManager.getUid(userManager.getIdFromSession(context));
-                boolean satBool = Boolean.valueOf(sat);
-                boolean sunBool = Boolean.valueOf(sun);
+                
                 database.executeUpdate("DELETE FROM goingout_apply WHERE uid='", uid, "'");
-                database.executeUpdate("INSERT INTO goingout_apply VALUES('", uid, "', '", satBool, "', '", sunBool, "')");
+                database.executeUpdate("INSERT INTO goingout_apply(uid, sat, sun) VALUES('", uid, "', ", sat, ", ", sun, ")");
                 
                 context.response().setStatusCode(201).end();
                 context.response().close();
             } catch (SQLException e) {
+            	e.printStackTrace();
                 context.response().setStatusCode(500).end();
                 context.response().close();
                 Log.l("SQLException");
--- application/dms-server/dms-core/src/main/java/com/dms/planb/template_routers/GoingoutDownloadRouter.java
+++ application/dms-server/dms-core/src/main/java/com/dms/planb/template_routers/GoingoutDownloadRouter.java
@@ -34,33 +34,34 @@ public class GoingoutDownloadRouter implements Handler<RoutingContext> {
 	@Override
 	public void handle(RoutingContext context) {
 		context = PrecedingWork.putHeaders(context);
-		
+
 		DataBase database = DataBase.getInstance();
 		SafeResultSet resultSet;
 		SafeResultSet stayStateResultSet;
 		SafeResultSet stayDefaultResultSet;
 		AES256 aes = UserManager.getAES();
-		
-		String week = (StringFormatter.format("%4d-%02d-%02d", context.request().getParam("year"), context.request().getParam("month"), context.request().getParam("week"))).getValue();
-		
+
+		String week = (StringFormatter.format("%4d-%02d-%02d", context.request().getParam("year"),
+				context.request().getParam("month"), context.request().getParam("week"))).getValue();
+
 		File file = getFile();
-		
+
 		try {
 			wb = new XSSFWorkbook(new FileInputStream(file));
-			
+
 			XSSFSheet sheet = wb.getSheetAt(0);
-			
-			for(Row row : sheet) {
-				for(Cell cell : row) {
-					switch(cell.getCellType()) {
+
+			for (Row row : sheet) {
+				for (Cell cell : row) {
+					switch (cell.getCellType()) {
 					case Cell.CELL_TYPE_NUMERIC:
 						StringBuilder sb = new StringBuilder(Double.toString(cell.getNumericCellValue()));
-						
+
 						String studentNumber = aes.encrypt(sb.toString().substring(0, 4));
-						
+
 						resultSet = database.executeQuery("SELECT * FROM student_data WHERE number='", studentNumber, "'");
-						
-						if(resultSet.next()) {
+
+						if (resultSet.next()) {
 							String uid = resultSet.getString("uid");
 							stayStateResultSet = database.executeQuery("SELECT * FROM stay_apply WHERE uid='", uid, "' AND week='", week, "'");
 
@@ -69,27 +70,28 @@ public class GoingoutDownloadRouter implements Handler<RoutingContext> {
 								setCellValues(stayStateResultSet, database, sheet, cell, uid);
 							} else {
 								// 잔류신청 정보 없음
-								stayDefaultResultSet = database.executeQuery("SELECT * FROM stay_apply_default WHERE uid='", uid, "'");
+								stayDefaultResultSet = database
+										.executeQuery("SELECT * FROM stay_apply_default WHERE uid='", uid, "'");
 								// default 값 조회
-								if(stayDefaultResultSet.next()) {
+								if (stayDefaultResultSet.next()) {
 									setCellValues(stayDefaultResultSet, database, sheet, cell, uid);
 								}
 							}
 						}
-						
+
 						break;
 					}
 				}
 			}
-			
+
 			FileOutputStream xlsToSave = new FileOutputStream(FILE_DIR + "외출신청.xlsx");
 			wb.write(xlsToSave);
 			xlsToSave.close();
-			
+
 			context.response().setStatusCode(200);
 			context.response().sendFile(FILE_DIR + "외출신청.xlsx");
 			context.response().close();
-		} catch(IOException | SQLException e) {
+		} catch (IOException | SQLException e) {
 			context.response().setStatusCode(500).end();
 			context.response().close();
 		}
@@ -102,33 +104,36 @@ public class GoingoutDownloadRouter implements Handler<RoutingContext> {
 		}
 		return file;
 	}
-	
+
 	private void setCellValues(SafeResultSet stayStateResultSet, DataBase database, XSSFSheet sheet, Cell cell, String uid) throws SQLException {
-		if(stayStateResultSet.getInt("value") == 4 || stayStateResultSet.getInt("value") == 3) {
-			// default 값이 잔류 또는 토요귀사일 때
+		if (stayStateResultSet.getInt("value") == 4 || stayStateResultSet.getInt("value") == 3) {
+			// 신청 상태 또는 default 값이 잔류 또는 토요귀사일 때
 			XSSFRow studentRow = sheet.getRow(cell.getRowIndex());
 			XSSFCell goingoutStateCell = studentRow.getCell(cell.getColumnIndex() + 2);
-			
-			SafeResultSet goingoutStateResultSet = database.executeQuery("SELECT * FROM goingout_apply WHERE uid='", uid, "'");
-			
-			goingoutStateResultSet.next();
-			boolean sat = goingoutStateResultSet.getBoolean("sat");
-			boolean sun = goingoutStateResultSet.getBoolean("sun");
 
-			if (sat && sun) {
-				// 모두 외출
-				goingoutStateCell.setCellValue("토요일, 일요일 외출");
-			} else if (sat && (!sun)) {
-				// 토요일만 외출
-				goingoutStateCell.setCellValue("토요일 외출");
-			} else if ((!sat) && sun) {
-				// 일요일만 외출
-				goingoutStateCell.setCellValue("일요일 외출");
+			SafeResultSet goingoutStateResultSet = database.executeQuery("SELECT * FROM goingout_apply WHERE uid='", uid, "'");
+
+			if (goingoutStateResultSet.next()) {
+				boolean sat = goingoutStateResultSet.getBoolean("sat");
+				boolean sun = goingoutStateResultSet.getBoolean("sun");
+
+				if (sat && sun) {
+					// 모두 외출
+					goingoutStateCell.setCellValue("토요일, 일요일 외출");
+				} else if (sat && (!sun)) {
+					// 토요일만 외출
+					goingoutStateCell.setCellValue("토요일 외출");
+				} else if ((!sat) && sun) {
+					// 일요일만 외출
+					goingoutStateCell.setCellValue("일요일 외출");
+				} else {
+					goingoutStateCell.setCellValue("미신청");
+				}
 			} else {
 				goingoutStateCell.setCellValue("미신청");
 			}
 		} else {
-			// default 값이 금요귀가 또는 토요귀가일 때
+			// 금요귀가 또는 토요귀가일 때
 			XSSFRow studentRow = sheet.getRow(cell.getRowIndex());
 			studentRow.getCell(cell.getColumnIndex()).setCellValue("");
 			studentRow.getCell(cell.getColumnIndex() + 1).setCellValue("");
