b2ca9db694a43df176f4cb5affe6dcc87e231f80
boxfox <rlatjdfo112@naver.com>
2017-03-30 14:41:28 +0900

    ResidualDownload Router 수정

--- application/dms-server/dms-templates/src/main/java/com/dms/boxfox/templates/xlsx/ResidualDownloadRouter.java
+++ application/dms-server/dms-templates/src/main/java/com/dms/boxfox/templates/xlsx/ResidualDownloadRouter.java
@@ -11,18 +11,18 @@ import org.boxfox.dms.utilities.log.Log;
 
 @RouteRegistration(path = "/stay/download", method = {HttpMethod.GET})
 public class ResidualDownloadRouter implements Handler<RoutingContext> {
-	private ResidualDownload residualDownload;
+    private ResidualDownload residualDownload;
 
-	public ResidualDownloadRouter(){
-		residualDownload = new ResidualDownload();
-	}
+    public ResidualDownloadRouter() {
+        residualDownload = new ResidualDownload();
+    }
 
-	public void handle(RoutingContext context) {
-		Log.l("Stay Download "+context.request().remoteAddress());
-		int year = Integer.valueOf(context.request().getParam("year"));
-		int month = Integer.valueOf(context.request().getParam("month"));
-		int week = Integer.valueOf(context.request().getParam("week"));
-		String date = StringFormatter.format("%4d-%02d-%02d", year, month, week).getValue();
-		context.response().sendFile(residualDownload.readExcel(date));
-	}
+    public void handle(RoutingContext context) {
+        Log.l("Stay Download " + context.request().remoteAddress());
+        int year = Integer.valueOf(context.request().getParam("year"));
+        int month = Integer.valueOf(context.request().getParam("month"));
+        int week = Integer.valueOf(context.request().getParam("week"));
+        String date = StringFormatter.format("%4d-%02d-%02d", year, month, week).getValue();
+        context.response().sendFile(residualDownload.readExcel(date));
+    }
 }
