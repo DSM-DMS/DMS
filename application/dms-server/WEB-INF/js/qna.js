f2c87d1b1633ab701b69a701bc1dd23410ce8b56
graykick <graykick@naver.com>
2017-04-04 16:55:36 +0900

    implement account reset
    implement account reset

--- application/dms-server/WEB-INF/js/qna.js
+++ application/dms-server/WEB-INF/js/qna.js
@@ -1,9 +1,12 @@
+getQnaCommentList(addTr);
+setCommentBtnEvent();
+setQnestionModifyEvent();
+
 function getProfilePicture(callback) {
     $.ajax({
         url: "/account/student",
         type: "GET",
-        data: {
-        },
+        data: {},
         success: function(data) {
             callback(data.profile_image);
         }
@@ -15,7 +18,7 @@ function getQnaCommentList(callback) {
         url: "/post/qna/comment",
         type: "GET",
         data: {
-            "no": no
+            "no": getAllUrlParams(document.URL).no
         },
         success: function(data) {
             callback(data);
@@ -24,82 +27,87 @@ function getQnaCommentList(callback) {
 }
 
 function addTr(data) {
-    var newComment = $('<div/>', {
-        class: "comment-box"
-    }).data("no", data.no);
+    var commentArr = JSON.parse(data).result;
 
-    var newProfile = $('<img/>', {
-        class: "profile comment-box-component",
-        text: data.writer;
-    }).appendTo(newComment);
-
-    var newWriter = $('<span/>', {
-        class: "comment-name comment-box-component",
-        text: data.writer;
-    }).appendTo(newComment);
-
-    var newDate = $('<span/>', {
-        class: "comment-date comment-box-component",
-        text: data.comment_date;
-    }).appendTo(newComment);
-
-    var newContent = $('<p/>', {
-        class: "comment-content comment-box-component",
-        text: data.comment_date;
-    }).appendTo(newComment);
-
-    var newModify = $('<span/>', {
-        class: "comment-modify comment-button comment-box-component",
-        text: "수정",
-        click: function() {
-            // 수정창 로드
-            $(newContent).hide(500);
-            var newModifyCommentInput = $('<textarea/>', {
-                class: "comment-modify-input comment-input-text",
+    for (var i = 0; i < commentArr.length; i++) {
+        (function(i) {
+            var newComment = $('<div/>', {
+                class: "comment-box"
+            }).data("no", commentArr[i].no);
+            var newProfile = $('<img/>', {
+                class: "profile comment-box-component",
+                text: commentArr[i].writer
             }).appendTo(newComment);
-            newModifyComment.val($(newContent).val());
 
-            var newModifyCommentBtn = $('<button/>', {
-                class: "comment-box-component comment-input-button",
+            var newWriter = $('<span/>', {
+                class: "comment-name comment-box-component",
+                text: commentArr[i].writer
+            }).appendTo(newComment);
+
+            var newDate = $('<span/>', {
+                class: "comment-date comment-box-component",
+                text: commentArr[i].comment_date
+            }).appendTo(newComment);
+
+            var newContent = $('<p/>', {
+                class: "comment-content comment-box-component",
+                text: commentArr[i].content
+            }).appendTo(newComment);
+
+            var newModify = $('<span/>', {
+                class: "comment-modify comment-button comment-box-component",
+                text: "수정",
+                click: function() {
+                    // 수정창 로드
+                    console.log("수정");
+                    $(newContent).hide(500);
+                    var newModifyCommentInput = $('<textarea/>', {
+                        class: "comment-modify-input comment-input-text",
+                    }).appendTo(newComment);
+                    newModifyCommentInput.val($(newContent).val());
+
+                    var newModifyCommentBtn = $('<button/>', {
+                        class: "comment-box-component comment-input-button",
+                        click: function() {
+                            $.ajax({
+                                url: "/post/qna/comment",
+                                type: "PATCH",
+                                data: {
+                                    "no": commentArr[i].no,
+                                    "content": newModifyCommentInput.val()
+                                },
+                                success: function(data) {
+                                    // 다시 qna로드
+                                }
+                            });
+                        }
+                    }).appendTo(newComment);
+                }
+            }).appendTo(newComment);
+            var newDelete = $('<span/>', {
+                class: "comment-delete comment-button comment-box-component",
+                text: "삭제",
                 click: function() {
                     $.ajax({
                         url: "/post/qna/comment",
-                        type: "PATCH",
+                        type: "DELETE",
                         data: {
-                            "no": data.no,
-                            "content": newModifyComment.val()
+                            "no": commentArr[i].no
                         },
                         success: function(data) {
                             // 다시 qna로드
                         }
                     });
                 }
-            })
-        }
-    }).appendTo(newComment);
+            }).appendTo(newComment);
+            newComment.appendTo("div.comment");
+        })(i);
+    }
 
-    var newDelete = $('<span/>', {
-        class: "comment-delete comment-button comment-box-component",
-        text: "삭제",
-        click: function() {
-            $.ajax({
-                url: "/post/qna/comment",
-                type: "DELETE",
-                data: {
-                    "no": data.no
-                },
-                success: function(data) {
-                    // 다시 qna로드
-                }
-            });
-        }
-    }).appendTo(newComment);
-
-    newComment.appendTo("div.comment");
 }
 
 function setQnestionModifyEvent() {
-    $(".question .content-modify").click(function () {
+    $(".question .content-modify").click(function() {
         // 수정페이지 요청
         $.ajax({
             url: "???",
@@ -108,92 +116,112 @@ function setQnestionModifyEvent() {
                 category: getAllUrlParams(document.URL).category,
                 type: "modify"
             },
-            success: function (data) {
+            success: function(data) {
                 redirect(data);
             },
-            error: function () {
-                alert("접근할 수 없어요 TT")
+            error: function() {
+                alert("접근할 수 없어요 TT");
             }
-        })
-    })
+        });
+    });
+}
+
+function setCommentBtnEvent() {
+    $(".comment-input-button").click(function() {
+        $.ajax({
+            url: "/post/qna/comment",
+            type: "POST",
+            data: {
+                no: getAllUrlParams(document.URL).no,
+                content: $(".comment-input-text").val(),
+                writer: "writer"
+            },
+            success: function(data) {
+                getQnaCommentList(addTr);
+            },
+            error: function() {
+                alert("접근할 수 없어요 TT");
+            }
+        });
+    });
 }
 
 function setQnestionDleteEvent() {
-    $(".question .content-delete").click(function () {
+    $(".question .content-delete").click(function() {
         // 수정페이지 요청
-    })
+    });
 }
 
 function setAnswerModifyEvent() {
-    $(".answer .content-modify").click(function () {
+    $(".answer .content-modify").click(function() {
         // 수정페이지 요청
-    })
+    });
 }
 
 function setAnswerModifyEvent() {
-    $(".answer .content-modify").click(function () {
+    $(".answer .content-modify").click(function() {
         // 수정페이지 요청
-    })
+    });
 }
 
 function getAllUrlParams(url) {
 
-  // get query string from url (optional) or window
-  var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+    // get query string from url (optional) or window
+    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
 
-  // we'll store the parameters here
-  var obj = {};
+    // we'll store the parameters here
+    var obj = {};
 
-  // if query string exists
-  if (queryString) {
+    // if query string exists
+    if (queryString) {
 
-    // stuff after # is not part of query string, so get rid of it
-    queryString = queryString.split('#')[0];
+        // stuff after # is not part of query string, so get rid of it
+        queryString = queryString.split('#')[0];
 
-    // split our query string into its component parts
-    var arr = queryString.split('&');
+        // split our query string into its component parts
+        var arr = queryString.split('&');
 
-    for (var i=0; i<arr.length; i++) {
-      // separate the keys and the values
-      var a = arr[i].split('=');
+        for (var i = 0; i < arr.length; i++) {
+            // separate the keys and the values
+            var a = arr[i].split('=');
 
-      // in case params look like: list[]=thing1&list[]=thing2
-      var paramNum = undefined;
-      var paramName = a[0].replace(/\[\d*\]/, function(v) {
-        paramNum = v.slice(1,-1);
-        return '';
-      });
+            // in case params look like: list[]=thing1&list[]=thing2
+            var paramNum = undefined;
+            var paramName = a[0].replace(/\[\d*\]/, function(v) {
+                paramNum = v.slice(1, -1);
+                return '';
+            });
 
-      // set parameter value (use 'true' if empty)
-      var paramValue = typeof(a[1])==='undefined' ? true : a[1];
+            // set parameter value (use 'true' if empty)
+            var paramValue = typeof(a[1]) === 'undefined' ? true : a[1];
 
-      // (optional) keep case consistent
-      paramName = paramName.toLowerCase();
-      paramValue = paramValue.toLowerCase();
+            // (optional) keep case consistent
+            paramName = paramName.toLowerCase();
+            paramValue = paramValue.toLowerCase();
 
-      // if parameter name already exists
-      if (obj[paramName]) {
-        // convert value to array (if still string)
-        if (typeof obj[paramName] === 'string') {
-          obj[paramName] = [obj[paramName]];
+            // if parameter name already exists
+            if (obj[paramName]) {
+                // convert value to array (if still string)
+                if (typeof obj[paramName] === 'string') {
+                    obj[paramName] = [obj[paramName]];
+                }
+                // if no array index number specified...
+                if (typeof paramNum === 'undefined') {
+                    // put the value on the end of the array
+                    obj[paramName].push(paramValue);
+                }
+                // if array index number specified...
+                else {
+                    // put the value at that index number
+                    obj[paramName][paramNum] = paramValue;
+                }
+            }
+            // if param name doesn't exist yet, set it
+            else {
+                obj[paramName] = paramValue;
+            }
         }
-        // if no array index number specified...
-        if (typeof paramNum === 'undefined') {
-          // put the value on the end of the array
-          obj[paramName].push(paramValue);
-        }
-        // if array index number specified...
-        else {
-          // put the value at that index number
-          obj[paramName][paramNum] = paramValue;
-        }
-      }
-      // if param name doesn't exist yet, set it
-      else {
-        obj[paramName] = paramValue;
-      }
     }
-  }
 
-  return obj;
+    return obj;
 }
