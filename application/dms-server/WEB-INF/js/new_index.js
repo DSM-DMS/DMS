8557383fcc47102115170a7feeb5836284324005
Ashton Yoon <y109384@naver.com>
2017-05-12 13:50:08 +0900

    implement notice view

--- application/dms-server/WEB-INF/js/new_index.js
+++ application/dms-server/WEB-INF/js/new_index.js
@@ -122,6 +122,7 @@ var $openPointButton = $(".point-btn");
 var $noticeMoreBtn = $(".notice-more");
 var $closeNoticeButton = $("#close-notice-window");
 var $noticeListWindow = $(".notice-window");
+var $noticeBox = $(".list-box");
 
 /**
  * Current state(stay)
@@ -205,6 +206,8 @@ $classSelect.on("click", "td", function(e) {
 });
 
 $openExtensionButton.on("click", function() {
+    $openStayButton.prop("disabled", true);
+    $openExtensionButton.prop("disabled", true);
     $panel.toggleClass("left-move");
     $extensionWindow.toggleClass("fade-in");
     $menu2.toggleClass("fade-out");
@@ -213,6 +216,8 @@ $openExtensionButton.on("click", function() {
 });
 
 $closeExtensionButton.on("click", function() {
+    $openStayButton.prop("disabled", false);
+    $openExtensionButton.prop("disabled", false);
     $panel.toggleClass("left-move");
     $extensionWindow.toggleClass("fade-in");
     $menu.toggleClass("fade-out");
@@ -269,11 +274,29 @@ function fillListCard(data, target) {
         "class": "list-box-no-title",
         text: data.title
     }));
+    newCard.append($('<p/>', {
+        "class": "list-box-writer",
+        text: "사감부"
+    }));
     // newCard.append($('<p/>', {
-    //     "class": "list-box-writer",
-    //     text: "사감부"
+    //     "class": "list-box-no-content",
+    //     html: data.content
     // }));
-
+    newCard.on('click', function(){
+        if(!$(".list-box p").hasClass("list-box-no-content")){
+            $(this).css('width', '100%');  
+            $(this).css('height', '50%'); 
+            $(this).append($('<p/>', {
+                "class": "list-box-no-content",
+                html: data.content
+            }));
+            $(".list-box-no-content").css('opacity', '1');
+        }else{
+            $(this).css('width', '20vh');  
+            $(this).css('height', '20vh'); 
+            $(this).children(".list-box-no-content").detach();
+        }
+    });
     target.append(newCard);
 }
 
@@ -295,7 +318,6 @@ function setNoticePreview() {
         }
     });
 }
-
 
 /** ======================================================================================
  * Dormitory rule
@@ -529,6 +551,8 @@ var setStayValue = function(thisDate) {
 setStayValue(stayDate)
 
 $openStayButton.click(function() {
+    $openStayButton.prop("disabled", true);
+    $openExtensionButton.prop("disabled", true);
     $stayWindow.toggleClass("fade-in");
     $panel.toggleClass("left-move");
     $menu.toggleClass("fade-out");
@@ -537,6 +561,8 @@ $openStayButton.click(function() {
 });
 
 $closeStayButton.on("click", function() {
+    $openStayButton.prop("disabled", false);
+    $openExtensionButton.prop("disabled", false);
     $panel.toggleClass("left-move");
     $stayWindow.toggleClass("fade-in");
     $menu.toggleClass("fade-out");
