69da2d889ee92dcbcbb0b6021fc4f7d5e85a2559
boxfox <rlatjdfo112@naver.com>
2017-05-21 21:37:10 +0900

    API Key request check

--- application/dms-server/dms-api/src/main/java/com/dsm/dms/secure/ApiRequestManager.java
+++ application/dms-server/dms-api/src/main/java/com/dsm/dms/secure/ApiRequestManager.java
@@ -1,13 +1,25 @@
 package com.dsm.dms.secure;
 
+import org.boxfox.dms.utilities.database.DataBase;
+import org.boxfox.dms.utilities.database.SafeResultSet;
+
+import java.sql.SQLException;
+
 /**
  * Created by user on 2017-05-10.
  */
 public class ApiRequestManager {
+    private DataBase database;
 
     public boolean checkRequestValid(String apiKey){
         boolean result = false;
-
+        try {
+            if(database.executeQuery("select count(*) from api_key where api_key='",apiKey,"'").nextAndReturn().getInt(1)==1){
+                result = true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return result;
     }
 }
