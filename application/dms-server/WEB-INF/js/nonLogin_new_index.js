a8ae7abeb83e817c7cc5310639de5cb3e6e9aa0a
graykick <graykick@naver.com>
2017-07-03 19:03:50 +0900

    change url of getNotice, getRule
    change url of getNotice, getRule
    
    1. notice: post/notice/list -> post/list/notice
    2. rule: post/rule/list -> post/list/rule

--- application/dms-server/WEB-INF/js/nonLogin_new_index.js
+++ application/dms-server/WEB-INF/js/nonLogin_new_index.js
@@ -820,11 +820,11 @@ $(document).ready(function() {
 	var slideWidth = $('#slider ul li').width();
 	var slideHeight = $('#slider ul li').height();
 	var sliderUlWidth = slideCount * slideWidth;
-	
+
 	$('#slider').css({ width: slideWidth, height: slideHeight });
-	
+
 	$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-	
+
     $('#slider ul li:last-child').prependTo('#slider ul');
 
     function moveLeft() {
