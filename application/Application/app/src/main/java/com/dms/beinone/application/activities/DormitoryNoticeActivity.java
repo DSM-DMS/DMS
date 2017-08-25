34e9d08922847308c00a4a54d5e53ce07ff777e3
Yun Jung Hyeon <yjh54240@naver.com>
2017-08-25 19:45:36 +0900

    공지사항 xml 추가

--- application/Application/app/src/main/java/com/dms/beinone/application/activities/DormitoryNoticeActivity.java
+++ application/Application/app/src/main/java/com/dms/beinone/application/activities/DormitoryNoticeActivity.java
@@ -2,14 +2,20 @@ package com.dms.beinone.application.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.dms.beinone.application.R;
 
 public class DormitoryNoticeActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dormitory_notice);
+
+        TextView TextView = (TextView) findViewById(R.id.tv_toolbar_title);
+        TextView.setText("공지사항");
     }
 }
--- application/Application/app/src/main/java/com/dms/beinone/application/fragments/DormitoryListFragment.java
+++ application/Application/app/src/main/java/com/dms/beinone/application/fragments/DormitoryListFragment.java
@@ -1,13 +1,17 @@
 package com.dms.beinone.application.fragments;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.dms.beinone.application.R;
+import com.dms.beinone.application.activities.DormitoryNoticeActivity;
 
 /**
  * Created by BeINone on 2017-05-31.
@@ -15,11 +19,27 @@ import com.dms.beinone.application.R;
 
 public class DormitoryListFragment extends Fragment {
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dormitory_list, container, false);
-
+        nextPage(view,getActivity());
         return view;
+    }
+
+
+
+    public void nextPage(View view, final Context context){
+        ImageButton imageButton=(ImageButton)view.findViewById(R.id.ib_dormitory_list_notice);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(context, DormitoryNoticeActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
