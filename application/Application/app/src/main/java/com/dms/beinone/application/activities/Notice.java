9340b9f9e075025e06294db0eb1167e556a36f1d
Yun Jung Hyeon <yjh54240@naver.com>
2017-08-26 20:07:11 +0900

    공지사항 디테일 화면 추가

--- application/Application/app/src/main/java/com/dms/beinone/application/activities/Notice.java
+++ application/Application/app/src/main/java/com/dms/beinone/application/activities/Notice.java
@@ -2,20 +2,18 @@ package com.dms.beinone.application.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import com.dms.beinone.application.R;
 
-public class DormitoryNoticeActivity extends AppCompatActivity {
-
+public class Notice extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dormitory_notice);
+        setContentView(R.layout.activity_notice);
 
         TextView TextView = (TextView) findViewById(R.id.tv_toolbar_title);
         TextView.setText("공지사항");
