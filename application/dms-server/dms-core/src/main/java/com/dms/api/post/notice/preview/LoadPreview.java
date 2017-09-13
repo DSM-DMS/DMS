980fa17b0c6c0688fe7c8ac8088906cf1522b4c4
PlanB <city7310@naver.com>
2017-09-13 09:23:37 +0900

    Fix loadpreview's SQLException

--- application/dms-server/dms-core/src/main/java/com/dms/api/post/notice/preview/LoadPreview.java
+++ application/dms-server/dms-core/src/main/java/com/dms/api/post/notice/preview/LoadPreview.java
@@ -1,40 +1,37 @@
 package com.dms.api.post.notice.preview;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.dms.utilities.database.DataBase;
-import com.dms.utilities.database.SafeResultSet;
-import com.dms.utilities.json.EasyJsonObject;
-import com.dms.utilities.log.Log;
+import org.json.JSONObject;
+
+import com.dms.utilities.database.DB;
 import com.dms.utilities.routing.Route;
 
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.RoutingContext;
 
-@Route(path="/post/notice/preivew", method={HttpMethod.GET})
+@Route(path="/post/notice/preview", method={HttpMethod.GET})
 public class LoadPreview implements Handler<RoutingContext> {
 	@Override
 	public void handle(RoutingContext ctx) {
-		DataBase database = DataBase.getInstance();
-		SafeResultSet resultSet;
-		SafeResultSet noticeSet;
-		EasyJsonObject responseObject = new EasyJsonObject();
+		JSONObject response = new JSONObject();
 		
 		try {
-			resultSet = database.executeQuery("SELECT * FROM notice_preview");
+			ResultSet previewSet = DB.executeQuery("SELECT * FROM notice_preview");
 			
-			if(resultSet.next()) {
-				int no = resultSet.getInt("no");
+			if(previewSet.next()) {
+				int no = previewSet.getInt("no");
 				
-				noticeSet = database.executeQuery("SELECT * FROM notice WHERE no=", no);
+				ResultSet noticeSet = DB.executeQuery("SELECT * FROM notice WHERE no=?", no);
 				
 				if(noticeSet.next()) {
-					responseObject.put("title", resultSet.getString("title"));
-					responseObject.put("content", resultSet.getString("content"));
+					response.put("title", noticeSet.getString("title"));
+					response.put("content", noticeSet.getString("content"));
 					
 					ctx.response().setStatusCode(200);
-					ctx.response().end(responseObject.toString());
+					ctx.response().end(response.toString());
 					ctx.response().close();
 				} else {
 					ctx.response().setStatusCode(204).end();
@@ -48,7 +45,7 @@ public class LoadPreview implements Handler<RoutingContext> {
 			ctx.response().setStatusCode(500).end();
 			ctx.response().close();
 			
-			Log.l("SQLException");
+			e.printStackTrace();
 		}
 	}
 }
