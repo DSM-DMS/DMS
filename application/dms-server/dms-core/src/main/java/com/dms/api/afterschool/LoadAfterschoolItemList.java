02098fa813e0b0290e26335622a38eaaae7bac70
PlanB <city7310@naver.com>
2017-09-15 13:00:25 +0900

    Refactir LoadAfterschoolItemList, UploadAfterschoolItem

--- application/dms-server/dms-core/src/main/java/com/dms/api/afterschool/LoadAfterschoolItemList.java
+++ application/dms-server/dms-core/src/main/java/com/dms/api/afterschool/LoadAfterschoolItemList.java
@@ -1,12 +1,12 @@
 package com.dms.api.afterschool;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.dms.account_manager.UserManager;
-import com.dms.utilities.database.DataBase;
-import com.dms.utilities.database.SafeResultSet;
-import com.dms.utilities.json.EasyJsonArray;
-import com.dms.utilities.json.EasyJsonObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.dms.utilities.database.DB;
 import com.dms.utilities.log.Log;
 import com.dms.utilities.routing.Route;
 
@@ -14,54 +14,40 @@ import io.vertx.core.Handler;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.RoutingContext;
 
-@Route(path="/apply/afterschool/item", method={HttpMethod.GET})
+@Route(path = "afterschool/item", method = { HttpMethod.GET })
 public class LoadAfterschoolItemList implements Handler<RoutingContext> {
-	public LoadAfterschoolItemList() {
-		
-	}
-	
 	@Override
 	public void handle(RoutingContext ctx) {
-
-		DataBase database = DataBase.getInstance();
-		SafeResultSet resultSet;
-		EasyJsonObject responseObject = new EasyJsonObject();
-		EasyJsonObject tempObject = new EasyJsonObject();
-		EasyJsonArray tempArray = new EasyJsonArray();
-		
 		try {
-			resultSet = database.executeQuery("SELECT * FROM afterschool_list");
-			
-			if(resultSet.next()) {
-				do {
-					tempObject = new EasyJsonObject();
-					
-					tempObject.put("no", resultSet.getInt("no"));
-					tempObject.put("title", resultSet.getString("title"));
-					tempObject.put("target", resultSet.getInt("target"));
-					tempObject.put("place", resultSet.getString("place"));
-					tempObject.put("on_monday", resultSet.getBoolean("on_monday"));
-					tempObject.put("on_tuesday", resultSet.getBoolean("on_tuesday"));
-					tempObject.put("on_wednesday", resultSet.getBoolean("on_wednesday"));
-					tempObject.put("on_saturday", resultSet.getBoolean("on_saturday"));
-					tempObject.put("instructor", resultSet.getString("instructor"));
-					tempObject.put("personnel", resultSet.getInt("personnel"));
+			ResultSet rs = DB.executeQuery("SELECT * FROM afterschool_list");
+			JSONArray response = new JSONArray();
 
-					tempArray.add(tempObject);
-				} while(resultSet.next());
-				
-				responseObject.put("result", tempArray);
-				
+			while (rs.next()) {
+				JSONObject afterschool = new JSONObject();
+
+				afterschool.put("no", rs.getInt("no"));
+				afterschool.put("title", rs.getString("title"));
+				// afterschool.put("target", rs.getInt("target"));
+				// afterschool.put("place", rs.getString("place"));
+				afterschool.put("on_monday", rs.getBoolean("on_monday"));
+				afterschool.put("on_tuesday", rs.getBoolean("on_tuesday"));
+				// afterschool.put("on_wednesday", rs.getBoolean("on_wednesday"));
+				afterschool.put("on_saturday", rs.getBoolean("on_saturday"));
+				// afterschool.put("instructor", rs.getString("instructor"));
+				// afterschool.put("personnel", rs.getInt("personnel"));
+
+				response.put(afterschool);
+			}
+			
+			if(response.length() != 0) {
 				ctx.response().setStatusCode(200);
-				ctx.response().end(responseObject.toString());
+				ctx.response().end(response.toString());
 				ctx.response().close();
 			} else {
 				ctx.response().setStatusCode(204).end();
 				ctx.response().close();
 			}
-		} catch(SQLException e) {
-			ctx.response().setStatusCode(500).end();
-			ctx.response().close();
+		} catch (SQLException e) {
 			Log.l("SQLException");
 		}
 	}
