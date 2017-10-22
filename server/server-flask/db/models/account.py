85c75a9a79c56f81748bd363218bb24f3e8c9d89
PlanB <city7310@naver.com>
2017-10-22 20:35:16 +0900

    [DONE] Account Models

--- server/server-flask/db/models/account.py
+++ server/server-flask/db/models/account.py
@@ -1,1 +1,23 @@
 from db.mongo import *
+
+
+class SignupRequiredModel(Document):
+    uid = StringField(primary_key=True)
+    name = StringField(required=True)
+    number = IntField(required=True)
+
+
+class AccountBase(Document):
+    id = StringField(primary_key=True)
+    pw = StringField(required=True)
+    name = StringField(required=True)
+    
+    meta = {'allow_inheritance': True}
+
+
+class StudentModel(AccountBase):
+    number = IntField(required=True)
+
+
+class AdminModel(AccountBase):
+    pass
