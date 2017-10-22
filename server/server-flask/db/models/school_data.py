08a70c3ebed9cb75ffe9e0154e7be8092006140d
PlanB <city7310@naver.com>
2017-10-22 21:49:01 +0900

    [DONE] School Data Models

--- server/server-flask/db/models/school_data.py
+++ server/server-flask/db/models/school_data.py
@@ -1,1 +1,13 @@
 from db.mongo import *
+
+
+class Meal(Document):
+    date = StringField(primary_key=True)
+    breakfast = DictField(required=True)
+    lunch = DictField(required=True)
+    dinner = DictField(required=True)
+
+
+class Schedule(Document):
+    date = StringField(primary_key=True)
+    schedules = ListField(required=True)
