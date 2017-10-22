de39ccf8a4f846d08709628c0f3f8481c250f210
PlanB <city7310@naver.com>
2017-10-22 20:26:37 +0900

    [UPDATE] Add logger to decorating functions

--- server/server-flask/preprocessor.py
+++ server/server-flask/preprocessor.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, current_app
 from flask_restful_swagger_2 import Api
+
+from logging import Formatter, INFO
+from logging.handlers import RotatingFileHandler
 
 
 def decorate(app):
@@ -9,23 +12,36 @@ def decorate(app):
     """
     @app.before_first_request
     def before_first_request():
-        pass
+        def make_logger():
+            handler = RotatingFileHandler('server_log.log', maxBytes=100000, backupCount=5)
+            handler.setFormatter(Formatter("[%(asctime)s] %(levelname)s - %(message)s"))
+
+            current_app.logger.addHandler(handler)
+            current_app.logger.setLevel(INFO)
+
+        make_logger()
+        current_app.logger.info('------ Logger Initialized ------')
 
     @app.before_request
     def before_request():
-        pass
+        current_app.logger.info('Requested from {0} [ {1} {2} ]'.format(request.host, request.method, request.url))
+        current_app.logger.info('Request values : {0}'.format(request.values))
 
     @app.after_request
     def after_request(response):
+        current_app.logger.info('Respond : {0}'.format(response.status))
+
         return response
 
     @app.teardown_request
     def teardown_request(exception):
-        pass
+        if not exception:
+            current_app.logger.info('Teardown request successfully.')
 
     @app.teardown_appcontext
     def teardown_appcontext(exception):
-        pass
+        if not exception:
+            current_app.logger.info('Teardown appcontext successfully.')
 
 
 def add_resources(app):
