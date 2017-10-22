4e21260a0d42c00be396cf96f97e4446cf55f4bc
PlanB <city7310@naver.com>
2017-10-22 21:42:44 +0900

    [DONE] Apply Models

--- server/server-flask/db/models/apply.py
+++ server/server-flask/db/models/apply.py
@@ -1,1 +1,20 @@
 from db.mongo import *
+from datetime import date
+
+
+class ApplyBase(EmbeddedDocument):
+    apply_date = StringField(required=True, default=str(date.today()))
+    meta = {'allow_inheritance': True}
+
+
+class AfterSchoolModel(ApplyBase):
+    no = IntField(required=True)
+
+
+class ExtensionModel(ApplyBase):
+    cls = IntField(required=True)
+    seat = IntField(required=True)
+
+
+class StayModel(ApplyBase):
+    value = IntField(required=True)
