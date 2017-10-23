5ac95edfdd7d5ac1ea54caeb1313cf3ce1d42475
PlanB <city7310@naver.com>
2017-10-23 11:28:46 +0900

    [DONE] Change Password, Change Number API

---
+++ server/server-flask/routes/api/student/account/after_signup.py
@@ -0,0 +1,32 @@
+from flask_restful_swagger_2 import Resource, request, swagger
+from flask_jwt import current_identity, jwt_required
+
+from db.models.account import StudentModel
+from . import after_signup_doc
+
+
+class ChangePW(Resource):
+    @swagger.doc(after_signup_doc.CHANGE_PW)
+    @jwt_required()
+    def post(self):
+        current_pw = request.form.get('current_pw')
+        new_pw = request.form.get('new_pw')
+
+        student = StudentModel.objects(id=current_identity, pw=current_pw).first()
+        if not student:
+            return '', 204
+        else:
+            student.update(pw=new_pw)
+
+            return '', 201
+
+
+class ChangeNumber(Resource):
+    @swagger.doc(after_signup_doc.CHANGE_NUMBER)
+    @jwt_required()
+    def post(self):
+        new_number = request.form.get('new_number')
+
+        StudentModel.objects(id=current_identity).first().update(number=new_number)
+
+        return '', 201
