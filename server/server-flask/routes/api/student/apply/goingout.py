26628ff346560bdf13f8cdf7a91aaacddb911bfd
Sangmin <sm372181@gmail.com>
2017-10-23 19:45:54 +0900

    [Done] Goingout api

---
+++ server/server-flask/routes/api/student/apply/goingout.py
@@ -0,0 +1,23 @@
+from flask_restful import Resource, request
+from flask_jwt import current_identity, jwt_required
+
+from db.models.account import StudentModel
+from db.models.apply import GoingoutApplyModel
+
+
+class Goingout(Resource):
+    @jwt_required()
+    def get(self):
+        student = StudentModel.objects(id=current_identity).first()
+
+        return {'sat': student.stay_applies.on_saturday,
+                'sun': student.stay_applies.on_sunday}, 200
+
+    @jwt_required()
+    def post(self):
+        sat = request.form.get('sat', type=bool)
+        sun = request.form.get('sun', type=bool)
+        student = StudentModel.objects(id=current_identity).first()
+        student.update(goingout_applies=GoingoutApplyModel(on_saturday=sat, on_sunday=sun))
+
+        return '', 201
