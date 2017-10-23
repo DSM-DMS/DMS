a730cbd8e6ff61c8022db59e1d308f5865314204
PlanB <city7310@naver.com>
2017-10-23 10:46:07 +0900

    [DONE] Meal API

---
+++ server/server-flask/routes/api/student/school_data/meal.py
@@ -0,0 +1,21 @@
+from flask_restful_swagger_2 import Resource, request, swagger
+
+from db.models.school_data import MealModel
+from . import meal_doc
+
+
+class Meal(Resource):
+    @swagger.doc(meal_doc.MEAL)
+    def get(self):
+        date = request.args.get('date')
+
+        meal = MealModel.objects(date=date).first()
+
+        if not meal:
+            return '', 204
+        else:
+            return {
+                'breakfast': meal.breakfast,
+                'lunch': meal.lunch,
+                'dinner': meal.dinner
+            }, 200
