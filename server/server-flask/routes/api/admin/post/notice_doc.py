564379d40f467a8e44a86bf597131e121b139171
PlanB <city7310@naver.com>
2017-10-24 00:16:03 +0900

    [DONE] Notice API Swagger

---
+++ server/server-flask/routes/api/admin/post/notice_doc.py
@@ -0,0 +1,89 @@
+NOTICE_POST = {
+    'tags': ['관리자 - 게시글'],
+    'description': '공지사항 업로드',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token',
+            'in': 'header',
+            'type': 'str'
+        },
+        {
+            'name': 'title',
+            'description': '공지사항 제목',
+            'in': 'formData',
+            'type': 'str'
+        },
+        {
+            'name': 'content',
+            'description': '공지사항 내용',
+            'in': 'formData',
+            'type': 'str'
+        }
+    ],
+    'responses': {
+        '201': {
+            'description': '공지사항 업로드 성공'
+        }
+    }
+}
+
+NOTICE_PATCH = {
+    'tags': ['관리자 - 게시글'],
+    'description': '공지사항 수정',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token',
+            'in': 'header',
+            'type': 'str'
+        },
+        {
+            'name': 'id',
+            'description': '수정할 공지사항 ID',
+            'in': 'formData',
+            'type': 'str'
+        },
+        {
+            'name': 'title',
+            'description': '공지사항 제목',
+            'in': 'formData',
+            'type': 'str'
+        },
+        {
+            'name': 'content',
+            'description': '공지사항 내용',
+            'in': 'formData',
+            'type': 'str'
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': '수정 성공'
+        }
+    }
+}
+
+NOTICE_DELETE = {
+    'tags': ['관리자 - 게시글'],
+    'description': '공지사항 삭제',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': 'JWT Token',
+            'in': 'header',
+            'type': 'str'
+        },
+        {
+            'name': 'id',
+            'description': '삭제할 공지사항 ID',
+            'in': 'formData',
+            'type': 'str'
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': '삭제 성공'
+        }
+    }
+}
