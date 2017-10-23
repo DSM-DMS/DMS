156adac921d7a1689e2d084006c6d70324eed88f
PlanB <city7310@naver.com>
2017-10-24 00:12:41 +0900

    [DONE] Account Initialize API

---
+++ server/server-flask/routes/api/admin/account/account_control.py
@@ -0,0 +1,26 @@
+from flask_restful_swagger_2 import Resource, request, swagger
+from flask_jwt import current_identity, jwt_required
+
+from db.models.account import AdminModel, StudentModel, SignupRequiredModel
+from . import account_control_doc
+
+
+class InitializeAccount(Resource):
+    @swagger.doc(account_control_doc.INITIALIZE_ACCOUNT)
+    @jwt_required()
+    def post(self):
+        if not AdminModel.objects(id=current_identity):
+            # Forbidden
+            return '', 403
+        else:
+            number = request.form.get('number', type=int)
+            # Number to initialize
+
+            student = StudentModel.objects(number=number).first()
+            # Get model
+            SignupRequiredModel(uuid=student.uuid, name=student.name, number=student.number).save()
+            # Move to 'signup required'
+            student.delete()
+            # Delete existing student account
+
+            return '', 201
