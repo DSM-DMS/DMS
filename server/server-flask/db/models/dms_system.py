09bf2c130be0d31c4efb7dc588bc24797053e947
PlanB <city7310@naver.com>
2017-10-23 10:48:28 +0900

    [DONE] Bug Report Model

--- server/server-flask/db/models/dms_system.py
+++ server/server-flask/db/models/dms_system.py
@@ -1,1 +1,10 @@
+from datetime import date
+
 from db.mongo import *
+
+
+class BugReportModel(Document):
+    title = StringField(required=True)
+    content = StringField(required=True)
+
+    report_date = StringField(required=True, default=str(date.today()))
