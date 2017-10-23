8d344adda9acab1c3f61a660e8da219adfbfb441
PlanB <city7310@naver.com>
2017-10-23 10:48:57 +0900

    [DONE] Post Base, FAQ&Notice&Rule Model

--- server/server-flask/db/models/post.py
+++ server/server-flask/db/models/post.py
@@ -1,1 +1,25 @@
+from datetime import date
+
 from db.mongo import *
+
+
+class PostBase(Document):
+    title = StringField(required=True)
+    content = StringField(required=True)
+    writer = StringField(required=True)
+    pinned = BooleanField(required=True, default=False)
+
+    write_date = StringField(required=True, default=str(date.today()))
+    meta = {'allow_inheritance': True}
+
+
+class FAQModel(PostBase):
+    pass
+
+
+class NoticeModel(PostBase):
+    pass
+
+
+class RuleModel(PostBase):
+    pass
