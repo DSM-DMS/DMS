7670d5667bf8031aa45cb3a671669d86c8e194d2
Sangmin <sm372181@gmail.com>
2017-10-25 21:53:17 +0900

    [FIX] admin stay check api

--- server/server-flask/routes/api/admin/apply/stay.py
+++ server/server-flask/routes/api/admin/apply/stay.py
@@ -15,22 +15,23 @@ class CheckStay(Resource):
 
         wb = openpyxl.load_workbook('명렬표.xlsx')
         ws = wb.active
-        status = ''
 
         for row in map(str, range(3, 68)):
             for column1, column2 in zip(['B', 'F', 'J', 'N'], ['D', 'H', 'L', 'P']):
                 student = StudentModel.objects(number=ws[column1+row]).first()
+                status = ''
+                value = student.stay_apply.value
 
                 if not student:
                     continue
 
-                if student.stay_apply.value == '1':
+                if value == '1':
                     status = '금요 귀가'
-                elif student.stay_apply.value == '2':
+                elif value == '2':
                     status = '토요 귀가'
-                elif student.stay_apply.value == '3':
+                elif value == '3':
                     status = '토요 귀사'
-                elif student.stay_apply.value == '4':
+                elif value == '4':
                     status = '잔류'
                 ws[column2+row] = status
 
