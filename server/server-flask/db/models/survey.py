f179c8a63462260b0825c1294c20c1be9ab865d2
PlanB <city7310@naver.com>
2017-10-26 11:38:29 +0900

    [UPDATE] Modify Survey API Structure

--- server/server-flask/db/models/survey.py
+++ server/server-flask/db/models/survey.py
@@ -4,7 +4,8 @@ from db.models.account import StudentModel
 from db.mongo import *
 
 
-class QuestionModel(EmbeddedDocument):
+class QuestionModel(Document):
+    survey_id = StringField(required=True)
     title = StringField(required=True)
     is_objective = BooleanField(required=True)
     choice_paper = ListField()
@@ -15,7 +16,6 @@ class SurveyModel(Document):
     start_date = StringField(required=True)
     end_date = StringField(required=True)
     target = ListField(IntField())
-    questions = ListField(EmbeddedDocumentField(QuestionModel))
 
     creation_date = StringField(required=True, default=str(date.today()))
 
