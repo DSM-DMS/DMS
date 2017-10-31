1476866cf21e058376c9ac2664b55aaa737b6775
PlanB <city7310@naver.com>
2017-11-01 08:50:16 +0900

    [REMOVE] resource.py, [UPDATE] Organize Config

--- server/server-flask/config.py
+++ server/server-flask/config.py
@@ -6,7 +6,7 @@ PORT = 3000
 API_VER = '0.1'
 API_TITLE = 'DMS'
 API_DESC = '''
-[ENDPOINT] http://dsm2015.cafe24.com:{0}
+[BASE URL] http://dsm2015.cafe24.com:{0}
 
 - Status Code 401 UNAUTHORIZED : JWT 토큰 만료됨, 또는 토큰이 정상적으로 전달되지 않음
 - Status Code 403 Forbidden : 권한 없음
