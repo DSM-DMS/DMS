7e3cb11bd26bea554155263e6f7db4526a0515ff
PlanB <city7310@naver.com>
2017-10-31 22:15:49 +0900

    [UPDATE] Split Authentication APIs

--- server/server-flask/routes/api/student/account/auth.py
+++ server/server-flask/routes/api/student/account/auth.py
@@ -4,32 +4,13 @@ import requests
 from flask_jwt_extended import create_access_token
 from flask_restful_swagger_2 import Resource, request, swagger
 
-from db.models.account import AdminModel, StudentModel
-from routes.swagger_docs.auth import authentication_doc
+from db.models.account import StudentModel
+from routes.swagger_docs.student.account import auth_doc
 from support import db_migrator
 
 
-class AdminAuth(Resource):
-    @swagger.doc(authentication_doc.AUTH_POST)
-    def post(self):
-        """
-        로그인
-        """
-        id = request.form.get('id')
-        pw = request.form.get('pw')
-
-        if id and pw and AdminModel.objects(id=id, pw=pw):
-            return {
-                'access_token': create_access_token(identity=id)
-            }, 201
-        else:
-            return {
-                'msg': 'Authentication Failed.'
-            }, 401
-
-
 class StudentAuth(Resource):
-    @swagger.doc(authentication_doc.AUTH_POST)
+    @swagger.doc(auth_doc.AUTH_POST)
     def post(self):
         """
         로그인
---
+++ server/server-flask/routes/swagger_docs/student/account/auth_doc.py
@@ -0,0 +1,31 @@
+AUTH_POST = {
+    'tags': ['계정'],
+    'description': '로그인',
+    'parameters': [
+        {
+            'name': 'id',
+            'description': 'ID',
+            'in': 'formData',
+            'type': 'str'
+        },
+        {
+            'name': 'pw',
+            'description': '비밀번호',
+            'in': 'formData',
+            'type': 'str'
+        }
+    ],
+    'responses': {
+        '201': {
+            'description': '로그인 성공',
+            'examples': {
+                'application/json': {
+                    'access_token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlkZW50aXR5IjoiYSIsInR5cGUiOiJhY2Nlc3MiLCJleHAiOjE1NDA1NTc0NDYsImp0aSI6ImJiN2M3MjJmLTZkZjMtNDljYy1iZTk5LWRkMjMzNDU1NDRjZSIsIm5iZiI6MTUwOTAyMTQ0NiwiaWF0IjoxNTA5MDIxNDQ2fQ.wmytxSuQlH-KjhxO2EzrIioWHWgEnyiqWpRBwWuM15M'
+                }
+            }
+        },
+        '204': {
+            'description': '로그인 실패'
+        }
+    }
+}
