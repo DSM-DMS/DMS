407b8eabd933d6186d4120bde2dc3c38d26dd358
JoMingyu <city7310@naver.com>
2017-11-03 10:15:16 +0900

    [UPDATE] Apply Required on Admin Survey API

--- server/server-flask/routes/api/admin/survey/survey_doc.py
+++ server/server-flask/routes/api/admin/survey/survey_doc.py
@@ -6,31 +6,36 @@ SURVEY_POST = {
             'name': 'Authorization',
             'description': 'JWT Token',
             'in': 'header',
-            'type': 'str'
+            'type': 'str',
+            'required': True
         },
         {
             'name': 'title',
             'description': '설문조사 제목',
             'in': 'formData',
-            'type': 'str'
+            'type': 'str',
+            'required': True
         },
         {
             'name': 'start_date',
             'description': '시작 날짜(YYYY-MM-DD)',
             'in': 'formData',
-            'type': 'str'
+            'type': 'str',
+            'required': True
         },
         {
             'name': 'end_date',
             'description': '종료 날짜(YYYY-MM-DD)',
             'in': 'formData',
-            'type': 'str'
+            'type': 'str',
+            'required': True
         },
         {
             'name': 'target',
             'description': '대상 학년',
             'in': 'formData',
-            'type': 'list'
+            'type': 'list',
+            'required': True
         }
     ],
     'responses': {
@@ -48,31 +53,36 @@ QUESTION_POST ={
             'name': 'Authorization',
             'description': 'JWT Token',
             'in': 'header',
-            'type': 'str'
+            'type': 'str',
+            'required': True
         },
         {
             'name': 'id',
             'description': '질문을 추가할 설문조사 ID',
             'in': 'formData',
-            'type': 'str'
+            'type': 'str',
+            'required': True
         },
         {
             'name': 'title',
             'description': '질문 제목',
             'in': 'formData',
-            'type': 'str'
+            'type': 'str',
+            'required': True
         },
         {
             'name': 'is_objective',
             'description': '객관식 여부',
             'in': 'formData',
-            'type': 'bool'
+            'type': 'bool',
+            'required': True
         },
         {
             'name': 'choice_paper',
-            'description': '객관식 선택지(optional)',
+            'description': '객관식 선택지',
             'in': 'formData',
-            'type': 'list'
+            'type': 'list',
+            'required': False
         }
     ],
     'responses': {
