844f32cd5d4daf4477a7d4a952fbc485789acf5e
PlanB <city7310@naver.com>
2017-11-12 22:19:57 +0900

    [UPDATE] Split Student Extension Apply API

--- server/server-flask/routes/api/student/apply/extension.py
+++ server/server-flask/routes/api/student/apply/extension.py
@@ -9,43 +9,83 @@ from db.models.apply import ExtensionApplyModel
 from routes.api.student.apply import extension_doc
 
 APPLY_START = time(17, 30)
-APPLY_END = time(22, 0)
+APPLY_END_11 = time(20, 30)
+APPLY_END_12 = time(22, 0)
 
 
-class Extension(Resource):
+class Extension11(Resource):
     uri = '/extension'
 
     @swagger.doc(extension_doc.EXTENSION_GET)
     @jwt_required
     def get(self):
         """
-        연장신청 정보 조회
+        11시 연장신청 정보 조회
         """
         student = StudentModel.objects(id=get_jwt_identity()).first()
 
-        if not student.extension_apply:
+        if not student.extension_apply_11:
             return Response('', 204)
 
         return {
-            'class': student.extension_apply.class_,
-            'seat': student.extension_apply.seat
+            'class': student.extension_apply_11.class_,
+            'seat': student.extension_apply_11.seat
         }, 200
 
     @swagger.doc(extension_doc.EXTENSION_POST)
     @jwt_required
     def post(self):
         """
-        연장신청
+        11시 연장신청
         """
         now = datetime.now().time()
 
-        if not APPLY_START < now < APPLY_END:
+        if not APPLY_START < now < APPLY_END_11:
             return Response('', 204)
 
         class_ = request.form.get('class', type=int)
         seat = request.form.get('seat', type=int)
 
         student = StudentModel.objects(id=get_jwt_identity()).first()
-        student.update(extension_apply=ExtensionApplyModel(class_=class_, seat=seat))
+        student.update(extension_apply_11=ExtensionApplyModel(class_=class_, seat=seat))
+
+        return Response('', 201)
+
+
+class Extension12(Resource):
+    uri = '/extension'
+
+    @swagger.doc(extension_doc.EXTENSION_GET)
+    @jwt_required
+    def get(self):
+        """
+        12시 연장신청 정보 조회
+        """
+        student = StudentModel.objects(id=get_jwt_identity()).first()
+
+        if not student.extension_apply_12:
+            return Response('', 204)
+
+        return {
+            'class': student.extension_apply_12.class_,
+            'seat': student.extension_apply_12.seat
+        }, 200
+
+    @swagger.doc(extension_doc.EXTENSION_POST)
+    @jwt_required
+    def post(self):
+        """
+        12시 연장신청
+        """
+        now = datetime.now().time()
+
+        if not APPLY_START < now < APPLY_END_12:
+            return Response('', 204)
+
+        class_ = request.form.get('class', type=int)
+        seat = request.form.get('seat', type=int)
+
+        student = StudentModel.objects(id=get_jwt_identity()).first()
+        student.update(extension_apply_12=ExtensionApplyModel(class_=class_, seat=seat))
 
         return Response('', 201)
