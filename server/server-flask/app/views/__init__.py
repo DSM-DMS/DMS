f81cc3d5dd49c6a7ba7142484d889c8ccf9e5b52
PlanB <city7310@naver.com>
2017-11-23 12:12:55 +0900

    [UPDATE] Apply Flassger on views __init__.py
    - Flask-restful-swagger-2 to Flask-restful & Flasgger

--- server/server-flask/app/views/__init__.py
+++ server/server-flask/app/views/__init__.py
@@ -1,13 +1,17 @@
 import pkgutil
 
-from flask import Blueprint
-from flask_restful_swagger_2 import Api
+from flask_restful import Api
+from flasgger import Swagger
 
+from app.docs import TEMPLATE
 from app.views import admin, student
 
 
-class Swagger(object):
+class ViewInjector(object):
     def __init__(self, app=None):
+        """
+        :type app: Flask
+        """
         self._global_resources = set()
 
         if app is not None:
@@ -28,14 +32,7 @@ class Swagger(object):
 
         return modules
 
-    def _factory(self, app, packages, bp_endpoint, url_prefix='', api_spec_url='/api/swagger', api_ver=None, api_title=None, api_desc=None):
-        api_ver = api_ver or app.config['API_VER']
-        api_title = api_title or app.config['API_TITLE']
-        api_desc = api_desc or app.config['API_DESC']
-
-        bp = Blueprint(bp_endpoint, __name__, url_prefix=url_prefix)
-        api = Api(bp, api_spec_url=api_spec_url, api_version=api_ver, title=api_title, description=api_desc)
-
+    def _factory(self, api, packages):
         resources = set()
 
         for loader, name in self._modules(packages):
@@ -51,13 +48,11 @@ class Swagger(object):
         for res in resources:
             api.add_resource(res, res.uri)
 
-        return bp
-
     def init_app(self, app):
-        bp_admin = self._factory(app, [admin], 'Admin', url_prefix='/admin', api_spec_url='/api/admin', api_title='DMS Admin API')
-        bp_student = self._factory(app, [student], 'Student', api_spec_url='/api/student', api_title='DMS Student API')
+        """
+        :type app: Flask
+        """
+        Swagger(app, template=TEMPLATE)
 
-        all_blueprints = (bp_admin, bp_student)
-
-        for bp in all_blueprints:
-            app.register_blueprint(bp)
+        api = Api(app)
+        self._factory(api, [admin, student])
