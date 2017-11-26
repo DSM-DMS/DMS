b5c7f2e5be4d7a5cb32e8d266e04b72e76983aac
unknown <geon0250@gmail.com>
2017-11-26 21:52:13 +0900

    getCookie 정의

--- application/dms-server/WEB-INF/template/js/nonLogin_new_index.js
+++ application/dms-server/WEB-INF/template/js/nonLogin_new_index.js
@@ -1024,3 +1024,19 @@ function setCookie(cname,cvalue,exdays) {
     var expires = "expires=" + d.toGMTString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
