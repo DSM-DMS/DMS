8b8f4e945113d3dacc8dc8203ec4f32cf057187a
PlanB <city7310@naver.com>
2017-11-28 19:06:07 +0900

    [FIX] Always 400 returning error on Account Migration Code

--- server/server-flask/app/views/student/account/auth.py
+++ server/server-flask/app/views/student/account/auth.py
@@ -23,16 +23,18 @@ class Auth(Resource):
         pw = request.form.get('pw')
 
         if id and pw and StudentModel.objects(id=id, pw=pw):
+            print('Branched Common way')
             return {
                 'access_token': create_access_token(identity=id)
             }, 201
         else:
+            print('Branched Migration way')
             # --- Migration code
             # Doesn't exist in MongoDB
-            result = requests.post('http://dsm2015.cafe24.com:8080/account/login/student', {'id': id, 'password': pw})
+            result = requests.post('http://localhost:8080/account/login/student', {'id': id, 'password': pw, 'remember': True})
             if result.status_code == 201:
                 # Login Success
-                student_data = json.loads(requests.get('http://dsm2015.cafe24.com:8080/info/student/id_based', params={'id': id}).text)
+                student_data = json.loads(requests.get('http://localhost:8080/info/student/id_based', params={'id': id}).text)
                 name, number, uuid = student_data['name'], student_data['number'], student_data['uuid']
 
                 StudentModel(id=id, pw=pw, name=name, number=number).save()
