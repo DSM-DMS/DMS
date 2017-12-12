e964e9b315eed6bf4cf51c895fb8e69e99f08fec
PlanB <city7310@naver.com>
2017-12-12 23:08:13 +0900

    [UPDATE] Refactor Auth API

--- server/server-flask/app/views/student/account/auth.py
+++ server/server-flask/app/views/student/account/auth.py
@@ -1,15 +1,11 @@
-import json
 from hashlib import sha256
 
-import requests
 from flask_jwt_extended import create_access_token
 from flask_restful import Resource, request
 from flasgger import swag_from
 
 from app.docs.student.account.auth import *
 from app.models.account import StudentModel
-
-from support import db_migrator
 
 
 class Auth(Resource):
@@ -28,28 +24,27 @@ class Auth(Resource):
         encrypted_pw = m.hexdigest()
 
         if StudentModel.objects(id=id, pw=encrypted_pw):
-            print('Branched Common way')
             return {
                 'access_token': create_access_token(identity=id)
             }, 201
         else:
-            print('Branched Migration way')
-            # --- Migration code
-            # Doesn't exist in MongoDB
-            result = requests.post('http://localhost:8080/account/login/student', {'id': id, 'password': pw, 'remember': True})
-            if result.status_code == 201:
-                # Login Success
-                student_data = json.loads(requests.get('http://localhost:8080/info/student/id_based', params={'id': id}).text)
-                name, number, uuid = student_data['name'], student_data['number'], student_data['uuid']
-
-                StudentModel(id=id, pw=encrypted_pw, name=name, number=number).save()
-
-                db_migrator.migrate_apply(id, uuid)
-
-                return {
-                    'access_token': create_access_token(identity=id)
-                }, 201
-            else:
-                return {
-                    'msg': 'Authentication Failed.'
-                }, 401
+            # print('Branched Migration way')
+            # # --- Migration code
+            # # Doesn't exist in MongoDB
+            # result = requests.post('http://localhost:8080/account/login/student', {'id': id, 'password': pw, 'remember': True})
+            # if result.status_code == 201:
+            #     # Login Success
+            #     student_data = json.loads(requests.get('http://localhost:8080/info/student/id_based', params={'id': id}).text)
+            #     name, number, uuid = student_data['name'], student_data['number'], student_data['uuid']
+            #
+            #     StudentModel(id=id, pw=encrypted_pw, name=name, number=number).save()
+            #
+            #     db_migrator.migrate_apply(id, uuid)
+            #
+            #     return {
+            #         'access_token': create_access_token(identity=id)
+            #     }, 201
+            # else:
+            return {
+                'msg': 'Authentication Failed.'
+            }, 401
