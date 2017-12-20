b94574ac17ccbb12f91e17290a6976392435b1c2
PlanB <city7310@naver.com>
2017-12-21 08:30:21 +0900

    [REMOVE] Web files

--- server/server-flask/app/middleware.py
+++ server/server-flask/app/middleware.py
@@ -10,33 +10,34 @@ class Logger(object):
             self.init_app(app)
 
     def init_app(self, app):
-        @app.before_first_request
-        def make_logger():
-            handler = RotatingFileHandler('server_log.log', maxBytes=100000, backupCount=5)
-            handler.setFormatter(Formatter("[%(asctime)s] %(levelname)s - %(message)s"))
-
-            current_app.logger.addHandler(handler)
-            current_app.logger.setLevel(INFO)
-
-            current_app.logger.info('------ Logger Initialized ------')
-
-        @app.before_request
-        def before_request():
-            current_app.logger.info('Requested from {0} [ {1} {2} ]'.format(request.host, request.method, request.url))
-            current_app.logger.info('Request values : {0}'.format(request.values))
-
-        @app.after_request
-        def after_request(response):
-            current_app.logger.info('Respond : {0}'.format(response.status))
-
-            response.headers['X-Powered-By'] = app.config['SERVICE_NAME']
-
-            return response
-
-        @app.teardown_appcontext
-        def teardown_appcontext(exception):
-            if not exception:
-                current_app.logger.info('Teardown appcontext successfully.')
+        pass
+        # @app.before_first_request
+        # def make_logger():
+        #     handler = RotatingFileHandler('server_log.log', maxBytes=100000, backupCount=5)
+        #     handler.setFormatter(Formatter("[%(asctime)s] %(levelname)s - %(message)s"))
+        #
+        #     current_app.logger.addHandler(handler)
+        #     current_app.logger.setLevel(INFO)
+        #
+        #     current_app.logger.info('------ Logger Initialized ------')
+        #
+        # @app.before_request
+        # def before_request():
+        #     current_app.logger.info('Requested from {0} [ {1} {2} ]'.format(request.host, request.method, request.url))
+        #     current_app.logger.info('Request values : {0}'.format(request.values))
+        #
+        # @app.after_request
+        # def after_request(response):
+        #     current_app.logger.info('Respond : {0}'.format(response.status))
+        #
+        #     response.headers['X-Powered-By'] = app.config['SERVICE_NAME']
+        #
+        #     return response
+        #
+        # @app.teardown_appcontext
+        # def teardown_appcontext(exception):
+        #     if not exception:
+        #         current_app.logger.info('Teardown appcontext successfully.')
 
 
 class ErrorHandler(object):
